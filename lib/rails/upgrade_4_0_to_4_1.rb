3c0db880dfbf1bf0d5232b8e150ee46fd4b16454
Richard Huang <flyerhzm@gmail.com>
2014-05-16 15:14:06 +0800

    add comments to upgrade_rails_4_0_to_4_1 snippet

--- lib/rails/upgrade_4_0_to_4_1.rb
+++ lib/rails/upgrade_4_0_to_4_1.rb
@@ -38,6 +38,7 @@ production:
   remove_file 'config/initializers/secret_token.rb'
 
   within_file 'test/test_helper.rb' do
+    # ActiveRecord::Migration.check_pending! => require 'test_help'
     with_node type: 'send', receiver: 'ActiveRecord::Migration', message: 'check_pending!' do
       replace_with "require 'test_help'"
     end
@@ -46,10 +47,12 @@ production:
   add_file 'config/initializers/cookies_serializer.rb', 'Rails.application.config.action_dispatch.cookies_serializer = :json'
 
   within_files '**/*.rb' do
+    # MultiJson.dump(obj) => obj.to_json
     with_node type: 'send', receiver: 'MultiJson', message: 'dump' do
       replace_with "{{arguments}}.to_json"
     end
 
+    # MultiJson.load(str) => JSON.parse(str)
     with_node type: 'send', receiver: 'MultiJson', message: 'load' do
       replace_with "JSON.parse {{arguments}}"
     end
@@ -57,6 +60,7 @@ production:
 
   within_files '**/*.rb' do
     [/before_/, /after_/].each do |message_regex|
+      # Warn if finding return in before_* or after_* callbacks
       within_node type: 'block', caller: {type: 'send', message: message_regex} do
         with_node type: 'return' do
           warn 'Using a return statement in an inline callback block causes a LocalJumpError to be raised when the callback is executed.'
