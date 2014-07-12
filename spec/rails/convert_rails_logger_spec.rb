0e76a63bb40512724ac50fd737eca0cf0f3c2219
Richard Huang <flyerhzm@gmail.com>
2014-07-12 17:25:09 +0800

    add "convert_rails_logger" snippet

---
+++ spec/rails/convert_rails_logger_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'Upgrade RAILS_DEFAULT_LOGGER to Rails.logger' do
+  before do
+    rewriter_path = File.join(File.dirname(__FILE__), '../../lib/rails/convert_rails_logger.rb')
+    @rewriter = eval(File.read(rewriter_path))
+  end
+
+  describe 'with fakefs', fakefs: true do
+    let(:constant_content) {"""
+RAILS_DEFAULT_LOGGER
+    """}
+    let(:constant_rewritten_content) {"""
+Rails.logger
+    """}
+
+    it 'converts' do
+      FileUtils.mkdir_p 'config/initializers'
+      File.write 'config/initializers/constant.rb', constant_content
+      @rewriter.process
+      expect(File.read 'config/initializers/constant.rb').to eq constant_rewritten_content
+    end
+  end
+end
