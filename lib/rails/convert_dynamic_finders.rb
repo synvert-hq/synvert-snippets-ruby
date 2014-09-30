d4823033af8c50bd7f2d87e174c66fe325a48ed3
Yong Gu <zerogy921@gmail.com>
2014-10-01 02:16:31 +0800

    Check attributes of schema.rb to decide whether to convert or not.

--- lib/rails/convert_dynamic_finders.rb
+++ lib/rails/convert_dynamic_finders.rb
@@ -8,10 +8,23 @@ It converts rails dynamic finders to arel syntax.
     scoped_by_... => where(...)
     find_or_initialize_by_... => find_or_initialize_by(...)
     find_or_create_by_... => find_or_create_by(...)
+
+But it keeps custom methods like dynamic finders intact.
   EOF
 
+  attributes = ["id"]
+  within_file 'db/schema.rb' do
+    within_node type: 'block', caller: {type: 'send', message: 'create_table'} do
+      with_node type: 'send', receiver: 't' do
+        attributes << node.arguments.first.to_value
+      end
+    end
+  end
+
   helper_method :dynamic_finder_to_hash do |prefix|
-    fields = node.message.to_s[prefix.length..-1].split("_and_")
+    fields = fields_of_dynamic_finder(prefix)
+    return nil if (fields & attributes) != fields
+
     if fields.length == node.arguments.length && :hash != node.arguments.first.type
       fields.length.times.map { |i|
         fields[i] + ": " + node.arguments[i].to_source
@@ -21,13 +34,17 @@ It converts rails dynamic finders to arel syntax.
     end
   end
 
+  helper_method :fields_of_dynamic_finder do |prefix|
+    node.message.to_s[prefix.length..-1].split("_and_")
+  end
+
   if_gem 'rails', {gte: '3.0.0'}
 
   within_files '**/*.rb' do
     # find_all_by_... => where(...)
     with_node type: 'send', message: /^find_all_by_/ do
       hash_params = dynamic_finder_to_hash("find_all_by_")
-      replace_with add_receiver_if_necessary("where(#{hash_params})")
+      replace_with(add_receiver_if_necessary("where(#{hash_params})")) if hash_params
     end
 
     # find_by_... => where(...).first
@@ -36,20 +53,20 @@ It converts rails dynamic finders to arel syntax.
         replace_with add_receiver_if_necessary("where(id: {{arguments}}).first")
       elsif :find_by_sql != node.message
         hash_params = dynamic_finder_to_hash("find_by_")
-        replace_with add_receiver_if_necessary("where(#{hash_params}).first")
+        replace_with(add_receiver_if_necessary("where(#{hash_params}).first")) if hash_params
       end
     end
 
     # find_last_by_... => where(...).last
     with_node type: 'send', message: /^find_last_by_/ do
       hash_params = dynamic_finder_to_hash("find_last_by_")
-      replace_with add_receiver_if_necessary("where(#{hash_params}).last")
+      replace_with(add_receiver_if_necessary("where(#{hash_params}).last")) if hash_params
     end
 
     # scoped_by_... => where(...)
     with_node type: 'send', message: /^scoped_by_/ do
       hash_params = dynamic_finder_to_hash("scoped_by_")
-      replace_with add_receiver_if_necessary("where(#{hash_params})")
+      replace_with(add_receiver_if_necessary("where(#{hash_params})")) if hash_params
     end
   end
 
@@ -59,13 +76,13 @@ It converts rails dynamic finders to arel syntax.
     # find_or_initialize_by_... => find_or_initialize_by(...)
     with_node type: 'send', message: /^find_or_initialize_by_/ do
       hash_params = dynamic_finder_to_hash("find_or_initialize_by_")
-      replace_with add_receiver_if_necessary("find_or_initialize_by(#{hash_params})")
+      replace_with(add_receiver_if_necessary("find_or_initialize_by(#{hash_params})")) if hash_params
     end
 
     # find_or_create_by_... => find_or_create_by(...)
     with_node type: 'send', message: /^find_or_create_by_/ do
       hash_params = dynamic_finder_to_hash("find_or_create_by_")
-      replace_with add_receiver_if_necessary("find_or_create_by(#{hash_params})")
+      replace_with(add_receiver_if_necessary("find_or_create_by(#{hash_params})")) if hash_params
     end
   end
 end
