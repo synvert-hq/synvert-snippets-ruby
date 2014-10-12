047b5b84b70c9559ddd4a537eec7d287e1ea795c
Richard Huang <flyerhzm@gmail.com>
2014-10-13 00:08:00 +0800

    fix ruby/use_symbol_to_proc snippet

--- lib/ruby/use_symbol_to_proc.rb
+++ lib/ruby/use_symbol_to_proc.rb
@@ -22,7 +22,7 @@ It uses &: (short for symbol to proc)
     %w(each map).each do |message|
       with_node type: 'block', caller: {message: message}, arguments: {size: 1} do
         argument_name = node.arguments.first.name.to_s
-        if_only_exist_node type: 'send', receiver: argument_name do
+        if_only_exist_node type: 'send', receiver: argument_name, arguments: {size: 0} do
           message = node.body.first.message
           replace_with "{{caller}}(&:#{message})"
         end
--- spec/ruby/use_symbol_to_proc_spec.rb
+++ spec/ruby/use_symbol_to_proc_spec.rb
@@ -9,10 +9,12 @@ describe 'Ruby uses symbol to proc' do
   describe 'with fakefs', fakefs: true do
     let(:test_content) {"
 (1..100).map { |i| i.to_s }
+enum.map { |e| e[:object_id] }
 (1..100).each { |i| i.to_s }
     "}
     let(:test_rewritten_content) {"
 (1..100).map(&:to_s)
+enum.map { |e| e[:object_id] }
 (1..100).each(&:to_s)
     "}
 
