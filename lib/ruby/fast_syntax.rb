45e81d57f5c039ade99f173403ae59e31a2f2bc0
Richard Huang <flyerhzm@gmail.com>
2014-10-13 13:06:43 +0800

    remove ruby/fetch_use_block-default snippet

--- lib/ruby/fast_syntax.rb
+++ lib/ruby/fast_syntax.rb
@@ -4,7 +4,6 @@ Use ruby fast syntax, it calls snippets
 
     avoid_parallel_assignment
     block_to_yield
-    fetch_use_block_default
     gsub_to_tr
     map_and_flatten_to_flat_map
     merge_to_square_brackets
@@ -17,7 +16,6 @@ Use ruby fast syntax, it calls snippets
   add_snippet 'ruby', 'block_to_yield'
   add_snippet 'ruby', 'fetch_use_block_default'
   add_snippet 'ruby', 'gsub_to_tr'
-  add_snippet 'ruby', 'map_and_flatten_to_flat_map'
   add_snippet 'ruby', 'merge_to_square_brackets'
   add_snippet 'ruby', 'use_symbol_to_proc'
 end
