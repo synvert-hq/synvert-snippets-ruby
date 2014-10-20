e7ce38ca097afb1f0c0e134f4e05e501c9e7a08c
Richard Huang <flyerhzm@gmail.com>
2014-10-20 18:15:27 +0800

    update ruby/fast_synvert snippet

--- lib/ruby/fast_syntax.rb
+++ lib/ruby/fast_syntax.rb
@@ -2,19 +2,20 @@ Synvert::Rewriter.new 'ruby', 'fast_syntax' do
   description <<-EOF
 Use ruby fast syntax, it calls snippets
 
-    avoid_parallel_assignment
     block_to_yield
     gsub_to_tr
     map_and_flatten_to_flat_map
     merge_to_square_brackets
+    parallel_assignment_to_sequential_assignment
     use_symbol_to_proc
 
     reference: https://speakerdeck.com/sferik/writing-fast-ruby
   EOF
 
-  add_snippet 'ruby', 'avoid_parallel_assignment'
   add_snippet 'ruby', 'block_to_yield'
   add_snippet 'ruby', 'gsub_to_tr'
+  add_snippet 'ruby', 'map_and_flatten_to_flat_map'
   add_snippet 'ruby', 'merge_to_square_brackets'
+  add_snippet 'ruby', 'parallel_assignment_to_sequential_assignment'
   add_snippet 'ruby', 'use_symbol_to_proc'
 end
