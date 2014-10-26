2eb8b496e7b256ba457de7cee27bdf10d59e204f
Richard Huang <flyerhzm@gmail.com>
2014-10-26 16:04:50 +0800

    add rspec/pending_to_skip snippet

---
+++ lib/rspec/pending_to_skip.rb
@@ -0,0 +1,101 @@
+Synvert::Rewriter.new 'rspec', 'pending_to_skip' do
+  description <<-EOF
+It converts rspec pending to skip.
+
+    it 'is skipped', :pending => true do
+      do_something_possibly_fail
+    end
+    =>
+    it 'is skipped', :skip => true do
+      do_something_possibly_fail
+    end
+
+    pending 'is skipped' do
+      do_something_possibly_fail
+    end
+    =>
+    skip 'is skipped' do
+      do_something_possibly_fail
+    end
+
+    it 'is skipped' do
+      pending
+      do_something_possibly_fail
+    end
+    =>
+    it 'is skipped' do
+      skip
+      do_something_possibly_fail
+    end
+
+    it 'is run and expected to fail' do
+      pending do
+        do_something_surely_fail
+      end
+    end
+    =>
+    it 'is run and expected to fail' do
+      skip
+      do_something_surely_fail
+    end
+  EOF
+
+  if_gem 'rspec', {gte: '3.0.0'}
+
+  within_files 'spec/**/*.rb' do
+    # it 'is run and expected to fail' do
+    #   pending do
+    #     do_something_surely_fail
+    #   end
+    # end
+    # =>
+    # it 'is run and expected to fail' do
+    #   skip
+    #   do_something_surely_fail
+    # end
+    with_node type: 'block', caller: {type: 'send', receiver: nil, message: 'pending', arguments: {size: 0}} do
+      replace_with "skip\n{{body}}"
+    end
+
+    # it 'is skipped' do
+    #   pending
+    #   do_something_possibly_fail
+    # end
+    # =>
+    # it 'is skipped' do
+    #   skip
+    #   do_something_possibly_fail
+    # end
+    with_node type: 'send', receiver: nil, message: 'pending', arguments: {size: 0} do
+      replace_with "skip"
+    end
+
+    # pending 'is skipped' do
+    #   do_something_possibly_fail
+    # end
+    # =>
+    # skip 'is skipped' do
+    #   do_something_possibly_fail
+    # end
+    with_node type: 'send', receiver: nil, message: 'pending', arguments: {size: 1, first: {type: 'str'}} do
+      replace_with "skip {{arguments}}"
+    end
+
+    %w(it describe context).each do |message|
+      # it 'is skipped', :pending => true do
+      #   do_something_possibly_fail
+      # end
+      # =>
+      # it 'is skipped', :skip => true do
+      #   do_something_possibly_fail
+      # end
+      with_node type: 'send', receiver: nil, message: message, arguments: {size: 2, last: {type: 'hash'}} do
+        goto_node 'arguments' do
+          goto_node 'last' do
+            replace_with node.to_source.sub('pending', 'skip')
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rspec/pending_to_skip_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe 'RSpec converts pending to skip' do
+  before do
+    rewriter_path = File.join(File.dirname(__FILE__), '../../lib/rspec/pending_to_skip.rb')
+    @rewriter = eval(File.read(rewriter_path))
+  end
+
+  describe 'with fakefs', fakefs: true do
+    let(:post_spec_content) {"
+  describe 'example' do
+    it 'is skipped', :pending => true do
+      do_something_possibly_fail
+    end
+
+    pending 'is skipped' do
+      do_something_possibly_fail
+    end
+
+    it 'is skipped' do
+      pending
+      do_something_possibly_fail
+    end
+
+    it 'is run and expected to fail' do
+      pending do
+        do_something_surely_fail
+      end
+    end
+  end
+    "}
+
+    let(:post_spec_rewritten_content) {"
+  describe 'example' do
+    it 'is skipped', :skip => true do
+      do_something_possibly_fail
+    end
+
+    skip 'is skipped' do
+      do_something_possibly_fail
+    end
+
+    it 'is skipped' do
+      skip
+      do_something_possibly_fail
+    end
+
+    it 'is run and expected to fail' do
+      skip
+      do_something_surely_fail
+    end
+  end
+    "}
+
+    it 'converts' do
+      FileUtils.mkdir_p 'spec/models'
+      File.write 'spec/models/post_spec.rb', post_spec_content
+      @rewriter.process
+      expect(File.read 'spec/models/post_spec.rb').to eq post_spec_rewritten_content
+    end
+  end
+end
