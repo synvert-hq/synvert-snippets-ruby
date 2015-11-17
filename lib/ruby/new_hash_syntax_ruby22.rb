86100b00b56b69a088e0b22672d5f30a5df99301
Richard Huang <flyerhzm@gmail.com>
2015-11-17 10:02:38 +0800

    use if_ruby dsl

--- lib/ruby/new_hash_syntax_ruby22.rb
+++ lib/ruby/new_hash_syntax_ruby22.rb
@@ -7,27 +7,25 @@ Use ruby new hash syntax extended in ruby 2.2.
     {:"foo-#{suffix}" 'bar'} => {"foo-#{suffix}": 'bar'}
   EOF
 
-  # Gem::Version initialize will strip RUBY_VERSION directly in ruby 1.9,
-  # which is solved from ruby 2.0.0, which calls dup internally.
-  if Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new("2.2.0")
-    within_files '**/*.rb' do
-      # {:foo => 'bar'} => {foo: 'bar'}
-      # {:'foo-x' => 'bar'} => {'foo-x': 'bar'}
-      # {:"foo-#{suffix}" 'bar'} => {"foo-#{suffix}": 'bar'}
-      within_node type: 'hash' do
-        with_node type: 'pair' do
-          case node.key.type
-          when :sym
-            case node.key.to_source
-            when /\A:"([^"'\\]*)"\z/
-              replace_with "'#{$1}': {{value}}"
-            when /\A:(.+)\z/
-              replace_with "#{$1}: {{value}}"
-            end
-          when :dsym
-            if new_key = node.key.to_source[/\A:(.+)/, 1]
-              replace_with "#{new_key}: {{value}}"
-            end
+  if_ruby '2.2.0'
+
+  within_files '**/*.rb' do
+    # {:foo => 'bar'} => {foo: 'bar'}
+    # {:'foo-x' => 'bar'} => {'foo-x': 'bar'}
+    # {:"foo-#{suffix}" 'bar'} => {"foo-#{suffix}": 'bar'}
+    within_node type: 'hash' do
+      with_node type: 'pair' do
+        case node.key.type
+        when :sym
+          case node.key.to_source
+          when /\A:"([^"'\\]*)"\z/
+            replace_with "'#{$1}': {{value}}"
+          when /\A:(.+)\z/
+            replace_with "#{$1}: {{value}}"
+          end
+        when :dsym
+          if new_key = node.key.to_source[/\A:(.+)/, 1]
+            replace_with "#{new_key}: {{value}}"
           end
         end
       end
--- lib/ruby/new_lambda_syntax.rb
+++ lib/ruby/new_lambda_syntax.rb
@@ -5,17 +5,15 @@ Use ruby new lambda syntax
     lambda { # do some thing } => -> { # do some thing }
   EOF
 
-  # Gem::Version initialize will strip RUBY_VERSION directly in ruby 1.9,
-  # which is solved from ruby 2.0.0, which calls dup internally.
-  if Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new("1.9.0")
-    within_files '**/*.rb' do
-      # lambda { |a, b, c| a + b + c } => ->(a, b, c) { a + b + c }
-      within_node type: 'block', caller: {type: 'send', message: 'lambda'} do
-        if node.arguments.empty?
-          replace_with "-> { {{body}} }"
-        else
-          replace_with "->({{arguments}}) { {{body}} }"
-        end
+  if_ruby '1.9.0'
+
+  within_files '**/*.rb' do
+    # lambda { |a, b, c| a + b + c } => ->(a, b, c) { a + b + c }
+    within_node type: 'block', caller: {type: 'send', message: 'lambda'} do
+      if node.arguments.empty?
+        replace_with "-> { {{body}} }"
+      else
+        replace_with "->({{arguments}}) { {{body}} }"
       end
     end
   end
