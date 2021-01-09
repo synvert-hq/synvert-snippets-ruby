60bb6a847684f2c2db80a74314c18503c29578b2
Richard Huang <flyerhzm@gmail.com>
2021-01-09 18:08:55 +0800

    add ruby snippet kernel_open_to_uri_open

---
+++ lib/ruby/kernel_open_to_uri_open.rb
@@ -0,0 +1,18 @@
+Synvert::Rewriter.new 'ruby', 'kernel_open_to_uri_open' do
+  description <<-EOF
+It converts Kernel#open to URI.open
+
+    open('http://test.com')
+    =>
+    URI.open('http://test.com')
+  EOF
+
+  within_files '**/*.rb' do
+    # open('http://test.com')
+    # =>
+    # URI.open('http://test.com')
+    with_node type: 'send', receiver: nil, message: 'open' do
+      replace_with 'URI.open({{arguments}})'
+    end
+  end
+end
