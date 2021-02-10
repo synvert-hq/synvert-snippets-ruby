2811a5dc0e8f16250f07319e7879a5a81f53cfee
Awesome Code <team@xinminlabs.com>
2021-02-10 01:36:23 +0000

    Auto corrected by following Lint Ruby Style/PerlBackrefs

--- lib/ruby/new_hash_syntax_ruby22.rb
+++ lib/ruby/new_hash_syntax_ruby22.rb
@@ -21,9 +21,9 @@ Synvert::Rewriter.new 'ruby', 'new_hash_syntax' do
         when :sym
           case node.key.to_source
           when /\A:"([^"'\\]*)"\z/
-            replace_with "'#{$1}': {{value}}"
+            replace_with "'#{Regexp.last_match(1)}': {{value}}"
           when /\A:(.+)\z/
-            replace_with "#{$1}: {{value}}"
+            replace_with "#{Regexp.last_match(1)}: {{value}}"
           end
         when :dsym
           if new_key = node.key.to_source[/\A:(.+)/, 1]
