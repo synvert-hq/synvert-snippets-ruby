7571e80b87f95a89a56ea6e4d2bde2ded525f6d4
Richard Huang <flyerhzm@gmail.com>
2021-02-15 12:05:51 +0800

    fix typo

--- lib/shoulda/use_new_syntax.rb
+++ lib/shoulda/use_new_syntax.rb
@@ -3,6 +3,6 @@
 Synvert::Rewriter.new 'shoulda', 'use_new_syntax' do
   description 'It calls shoulda/use_matcher_syntax and shoulda/fix_deprecations snippets.'
 
-  add_snippet 'shoulda', 'use_short_syntax'
+  add_snippet 'shoulda', 'use_matcher_syntax'
   add_snippet 'shoulda', 'fix_deprecations'
 end
