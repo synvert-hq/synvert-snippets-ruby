82455d2b6c00a96516141e249430f94de17fda2e
Richard Huang <flyerhzm@gmail.com>
2021-02-17 11:52:49 +0800

    add rails/convert_after_commit snippet

---
+++ lib/rails/convert_after_commit.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+Synvert::Rewriter.new 'rails', 'convert_after_commit' do
+  description <<~EOS
+    It converts active_record after_commit.
+
+    ```ruby
+    after_commit :add_to_index_later, on: :create
+    after_commit :update_in_index_later, on: :update
+    after_commit :remove_from_index_later, on: :destroy
+    ```
+    =>
+
+    ```ruby
+    after_create_commit :add_to_index_later
+    after_update_commit :update_in_index_later
+    after_detroy_commit :remove_from_index_later
+    ```
+  EOS
+
+  if_gem 'active_record', { gte: '5.0.0' }
+
+  within_files 'app/models/**/*.rb' do
+    # after_commit :add_to_index_later, on: :create
+    # after_commit :update_in_index_later, on: :update
+    # after_commit :remove_from_index_later, on: :destroy
+    # =>
+    # after_create_commit :add_to_index_later
+    # after_update_commit :update_in_index_later
+    # after_detroy_commit :remove_from_index_later
+    with_node type: 'send', receiver: nil, message: 'after_commit', arguments: { size: 2 } do
+      options = node.arguments.last
+      if options.has_key?(:on)
+        other_options = options.children.reject { |pair_node| pair_node.key.to_value == :on }
+        if other_options.empty?
+          replace_with "after_#{options.hash_value(:on).to_value}_commit {{arguments.first.to_source}}"
+        else
+          replace_with "after_#{options.hash_value(:on).to_value}_commit {{arguments.first.to_source}}, #{other_options.map(&:to_source).join(', ')}"
+        end
+      end
+    end
+  end
+end
---
+++ spec/rails/convert_after_commit_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Convert after_commit' do
+  let(:rewriter_name) { 'rails/convert_after_commit' }
+  let(:fake_file_path) { 'app/models/post.rb' }
+  let(:test_content) { '
+class Post < ApplicationRecord
+  after_commit :add_to_index_later, on: :create, if: :can_add?
+  after_commit :update_in_index_later, on: :update
+  after_commit :remove_from_index_later, on: :destroy
+end
+  '}
+  let(:test_rewritten_content) { '
+class Post < ApplicationRecord
+  after_create_commit :add_to_index_later, if: :can_add?
+  after_update_commit :update_in_index_later
+  after_destroy_commit :remove_from_index_later
+end
+  '}
+
+  include_examples 'convertable'
+end
