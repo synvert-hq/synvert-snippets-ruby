de940ae519f21784309b8a184e44876319975fcf
Richard Huang <flyerhzm@gmail.com>
2021-02-17 11:17:37 +0800

    add rails/fix_4_0_deprecations snippet

---
+++ lib/rails/fix_4_0_deprecations.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+Synvert::Rewriter.new 'rails', 'fix_4_0_deprecations' do
+  description <<~EOS
+    It fixes rails 4.0 deprecations.
+
+    ```ruby
+    ActiveRecord::Fixtures
+    ActiveRecord::TestCase
+    ActionController::Integration
+    ActionController::IntegrationTest
+    ActionController::PerformanceTest
+    ActionController::AbstractRequest
+    ActionController::Request
+    ActionController::AbstractResponse
+    ActionController::Response
+    ActionController::Routing
+    ```
+
+    =>
+
+    ```ruby
+    ActiveRecord::FixtureSet
+    ActiveSupport::TestCase
+    ActionDispatch::Integration
+    ActionDispatch::IntegrationTest
+    ActionDispatch::PerformanceTest
+    ActionDispatch::Request
+    ActionDispatch::Request
+    ActionDispatch::Response
+    ActionDispatch::Response
+    ActionDispatch::Routing
+    ```
+  EOS
+
+  if_gem 'rails', { gte: '4.0.0' }
+
+  within_files '**/*.rb' do
+    { 'ActiveRecord::Fixtures' => 'ActiveRecord::FixtureSet',
+     'ActiveRecord::TestCase' => 'ActiveSupport::TestCase',
+     'ActionController::Integration' => 'ActionDispatch::Integration',
+     'ActionController::IntegrationTest' => 'ActionDispatch::IntegrationTest',
+     'ActionController::PerformanceTest' => 'ActionDispatch::PerformanceTest',
+     'ActionController::AbstractRequest' => 'ActionDispatch::Request',
+     'ActionController::Request' => 'ActionDispatch::Request',
+     'ActionController::AbstractResponse' => 'ActionDispatch::Response',
+     'ActionController::Response' => 'ActionDispatch::Response',
+     'ActionController::Routing' => 'ActionDispatch::Routing' }.each do |deprecated, favor|
+      with_node to_source: deprecated do
+        replace_with favor
+      end
+    end
+  end
+end
---
+++ spec/rails/fix_4_0_deprecations_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Fix rails 4.0 deprecations' do
+  let(:rewriter_name) { 'rails/fix_4_0_deprecations' }
+  let(:fake_file_path) { 'test/unit/post_test.rb' }
+  let(:test_content) { '
+require "test_helper"
+
+class PostTest < ActiveRecord::TestCase
+  def constants
+    [ActionController::Integration, ActionController::IntegrationTest, ActionController::PerformanceTest, ActionController::AbstractRequest,
+    ActionController::Request, ActionController::AbstractResponse, ActionController::Response, ActionController::Routing]
+  end
+end
+  '}
+  let(:test_rewritten_content) { '
+require "test_helper"
+
+class PostTest < ActiveSupport::TestCase
+  def constants
+    [ActionDispatch::Integration, ActionDispatch::IntegrationTest, ActionDispatch::PerformanceTest, ActionDispatch::Request,
+    ActionDispatch::Request, ActionDispatch::Response, ActionDispatch::Response, ActionDispatch::Routing]
+  end
+end
+  '}
+
+  include_examples 'convertable'
+end
