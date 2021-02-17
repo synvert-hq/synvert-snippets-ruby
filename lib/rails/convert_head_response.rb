20a8fa4d3dc2682e659fb38b7983eadd8883ba20
Richard Huang <flyerhzm@gmail.com>
2021-02-17 13:34:22 +0800

    rename rails/convert_render_nothing_true_to_head_ok to rails/convert_head_response

---
+++ lib/rails/convert_head_response.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+Synvert::Rewriter.new 'rails', 'convert_head_response' do
+  description <<~EOS
+    It replaces render head response in controller files.
+
+    ```ruby
+    render nothing: true
+    render nothing: true, status: :created
+
+    head status: 406
+    head location: '/foo'
+    ```
+
+    =>
+
+    ```ruby
+    head :ok
+    head :created
+
+    head 406
+    head :ok, location: '/foo'
+    ```
+  EOS
+
+  within_file 'app/controllers/**/*.rb' do
+    # render nothing: true
+    # render nothing: true, status: :created
+    # =>
+    # head :ok
+    # head :created
+    with_node type: 'send', receiver: nil, message: 'render', arguments: { size: 1, first: { type: 'hash' } } do
+      hash_node = node.arguments.first
+      if hash_node.has_key?(:nothing) && hash_node.hash_value(:nothing).to_value == true
+        status_value = hash_node.hash_value(:status) ? hash_node.hash_value(:status).to_source : ':ok'
+        replace_with "head #{status_value}"
+      end
+    end
+
+    # head status: 406
+    # head location: '/foo'
+    # =>
+    # head 406
+    # head :ok, location: '/foo'
+    with_node type: 'send', receiver: nil, message: 'head', arguments: { size: 1, first: { type: 'hash' } } do
+      if node.arguments.first.has_key? :status
+        replace_with 'head {{arguments.first.values.first}}'
+      else
+        replace_with 'head :ok, {{arguments}}'
+      end
+    end
+  end
+end
--- spec/rails/convert_head_response_spec.rb
+++ spec/rails/convert_head_response_spec.rb
@@ -2,11 +2,15 @@
 
 require 'spec_helper'
 
-RSpec.describe 'Convert render nothing: true to head :ok' do
-  let(:rewriter_name) { 'rails/convert_render_nothing_true_to_head_ok' }
+RSpec.describe 'Convert head response' do
+  let(:rewriter_name) { 'rails/convert_head_response' }
   let(:fake_file_path) { 'app/controllers/posts_controller.rb' }
   let(:test_content) { '
 class PostsController < ApplicationController
+  rescue_from BadGateway do
+    head status: 502
+  end
+
   def ok
     render nothing: true
   end
@@ -14,16 +18,28 @@ class PostsController < ApplicationController
   def created
     render nothing: true, status: :created
   end
+
+  def redirect
+    head location: "/foo"
+  end
 end
   '}
   let(:test_rewritten_content) { '
 class PostsController < ApplicationController
+  rescue_from BadGateway do
+    head 502
+  end
+
   def ok
     head :ok
   end
 
   def created
     head :created
+  end
+
+  def redirect
+    head :ok, location: "/foo"
   end
 end
   '}
