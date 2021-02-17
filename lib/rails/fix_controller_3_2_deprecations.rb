94eb550191c42a67f5bcf32f790286c9863b592c
Richard Huang <flyerhzm@gmail.com>
2021-02-17 11:03:44 +0800

    add rails/fix_controller_3_2_deprecations snippet

---
+++ lib/rails/fix_controller_3_2_deprecations.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+Synvert::Rewriter.new 'rails', 'fix_controller_3_2_deprecations' do
+  description <<~EOS
+    It fixes rails 3.2 controller deprecations.
+
+    ```ruby
+    ActionController::UnknownAction
+    ```
+
+    =>
+
+    ```ruby
+    AbstractController::ActionNotFound
+    ```
+
+    ```ruby
+    ActionController::DoubleRenderError
+    ```
+
+    =>
+
+    ```ruby
+    AbstractController::DoubleRenderError
+    ```
+  EOS
+
+  if_gem 'actionpack', { gte: '3.2.0' }
+
+  within_files 'app/controllers/**/*.rb' do
+    # ActionController::UnknownAction => AbstractController::ActionNotFound
+    # ActionController::DoubleRenderError => AbstractController::DoubleRenderError
+    { 'ActionController::UnknownAction' => 'AbstractController::ActionNotFound', 'ActionController::DoubleRenderError' => 'AbstractController::DoubleRenderError' }.each do |old_const, new_const|
+      with_node type: 'const', to_source: old_const do
+        replace_with new_const
+      end
+    end
+  end
+end
---
+++ spec/rails/fix_controller_3_2_deprecations_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Fix rails controller 3.2 deprecations' do
+  let(:rewriter_name) { 'rails/fix_controller_3_2_deprecations' }
+  let(:fake_file_path) { 'app/controllers/application_controller.rb' }
+  let(:test_content) { '
+class ApplicationController < ActionController::Base
+  rescue_from ActionController::UnknownAction, :with => :render_404
+end
+  '}
+  let(:test_rewritten_content) { '
+class ApplicationController < ActionController::Base
+  rescue_from AbstractController::ActionNotFound, :with => :render_404
+end
+  '}
+
+  include_examples 'convertable'
+end
