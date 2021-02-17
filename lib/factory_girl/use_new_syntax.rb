c6e7dd578d59acf3f151b9408d98d06bd9779203
Richard Huang <flyerhzm@gmail.com>
2021-02-17 10:59:39 +0800

    rename factory_girl/use_new_syntax to factory_girl/use_2_0_new_syntax

--- lib/factory_girl/use_new_syntax.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Synvert::Rewriter.new 'factory_girl', 'use_new_syntax' do
-  description 'It calls factory_girl/use_short_syntax and factory_girl/fix_deprecations snippets.'
-
-  add_snippet 'factory_girl', 'use_short_syntax'
-  add_snippet 'factory_girl', 'fix_deprecations'
-end
