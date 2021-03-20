ee6ba2d1e2b1fed9f371c95bb1ba2f5ed5b5b292
Awesome Code <team@xinminlabs.com>
2021-03-20 02:44:56 +0000

    Auto corrected by following Lint Ruby Layout/HeredocIndentation

--- spec/rails/convert_render_text_to_render_plain_spec.rb
+++ spec/rails/convert_render_text_to_render_plain_spec.rb
@@ -6,26 +6,26 @@ RSpec.describe 'Convert render :text to render :plain' do
   let(:rewriter_name) { 'rails/convert_render_text_to_render_plain' }
   let(:fake_file_path) { 'app/controllers/posts_controller.rb' }
   let(:test_content) { <<~EOS }
-      class PostsController < ApplicationController
-        def foo
-          render text: 'OK'
-        end
-
-        def bar
-          render text: 'Not OK', status: 403
-        end
+    class PostsController < ApplicationController
+      def foo
+        render text: 'OK'
       end
+
+      def bar
+        render text: 'Not OK', status: 403
+      end
+    end
     EOS
   let(:test_rewritten_content) { <<~EOS }
-      class PostsController < ApplicationController
-        def foo
-          render plain: 'OK'
-        end
-
-        def bar
-          render plain: 'Not OK', status: 403
-        end
+    class PostsController < ApplicationController
+      def foo
+        render plain: 'OK'
       end
+
+      def bar
+        render plain: 'Not OK', status: 403
+      end
+    end
     EOS
 
   include_examples 'convertable'
