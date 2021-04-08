6567933f684e5a6ae5c820b26824504c0b55ec48
Richard Huang <flyerhzm@gmail.com>
2021-04-08 20:38:49 +0800

    delete warn snippets

--- lib/rails/convert_rails_test_xhr_4_2_to_5_0.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Synvert::Rewriter.new 'rails', 'convert_rails_test_xhr_4_2_to_5_0' do
-  description "It's deprecated, please use `rails/convert_rails_test_request_methods_4_2_to_5_0` instead"
-  warn "It's deprecated, please use `rails/convert_rails_test_request_methods_4_2_to_5_0` instead"
-end
--- lib/rails/convert_render_nothing_true_to_head_ok.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Synvert::Rewriter.new 'rails', 'convert_render_nothing_true_to_head_ok' do
-  description "It's deprecated, please use `rails/convert_head_response` instead"
-  warn "It's deprecated, please use `rails/convert_head_response` instead"
-end
