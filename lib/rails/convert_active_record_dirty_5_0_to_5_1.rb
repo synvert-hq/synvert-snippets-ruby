39430c39ffff0e06cf01f68c0d5b8ce10a9ba0d8
Richard Huang <flyerhzm@gmail.com>
2021-04-15 13:36:42 +0800

    fix convert active record dirty for observers

--- lib/rails/convert_active_record_dirty_5_0_to_5_1.rb
+++ lib/rails/convert_active_record_dirty_5_0_to_5_1.rb
@@ -88,8 +88,9 @@ Synvert::Rewriter.new 'rails', 'convert_active_record_dirty_5_0_to_5_1' do
   ]
 
   # convert ActiveRecord::Dirty api change
-  helper_method :convert_dirty_api_change do |before_name, after_name, attributes|
-    # after_save :invalidate_cache, if: :status_changed?
+  #
+  # after_save :invalidate_cache, if: :status_changed?
+  helper_method :convert_sym_dirty_api_change do |before_name, after_name, attributes|
     with_node type: 'sym', to_value: before_name do
       if before_name.is_a?(Regexp)
         if node.to_value =~ before_name && attributes.include?(Regexp.last_match(1))
@@ -99,16 +100,20 @@ Synvert::Rewriter.new 'rails', 'convert_active_record_dirty_5_0_to_5_1' do
         replace_with after_name
       end
     end
+  end
 
-    # after_save :invalidate_cache, if: -> { title_changed? || summary_chagned? }
-    #
-    # or
-    #
-    # after_save :invalidate_cache
-    # def invalidate_cache
-    #   if title_chagned? || summary_changed?
-    # . end
-    # end
+  # convert ActiveRecord::Dirty api change
+  #
+  # after_save :invalidate_cache, if: -> { title_changed? || summary_chagned? }
+  #
+  # or
+  #
+  # after_save :invalidate_cache
+  # def invalidate_cache
+  #   if title_chagned? || summary_changed?
+  # . end
+  # end
+  helper_method :convert_send_dirty_api_change do |before_name, after_name, attributes|
     with_node type: 'send', message: before_name do
       if before_name.is_a?(Regexp)
         if node.message.to_s =~ before_name && attributes.include?(Regexp.last_match(1))
@@ -131,7 +136,8 @@ Synvert::Rewriter.new 'rails', 'convert_active_record_dirty_5_0_to_5_1' do
       with_node type: 'send', receiver: nil, message: callback_name do
         custom_callback_names << node.arguments[0].to_value if !node.arguments.empty? && node.arguments[0].type == :sym
         callback_changes.each do |before_name, after_name|
-          convert_dirty_api_change(before_name, after_name, attributes)
+          convert_sym_dirty_api_change(before_name, after_name, attributes)
+          convert_send_dirty_api_change(before_name, after_name, attributes)
         end
       end
 
@@ -143,7 +149,7 @@ Synvert::Rewriter.new 'rails', 'convert_active_record_dirty_5_0_to_5_1' do
       #     end
       with_node type: 'block', caller: { type: 'send', receiver: nil, message: callback_name } do
         callback_changes.each do |before_name, after_name|
-          convert_dirty_api_change(before_name, after_name, attributes)
+          convert_send_dirty_api_change(before_name, after_name, attributes)
         end
       end
     end
@@ -156,7 +162,7 @@ Synvert::Rewriter.new 'rails', 'convert_active_record_dirty_5_0_to_5_1' do
     with_node type: 'def' do
       if callback_names.include?(node.name) || custom_callback_names.include?(node.name)
         callback_changes.each do |before_name, after_name|
-          convert_dirty_api_change(before_name, after_name, attributes)
+          convert_send_dirty_api_change(before_name, after_name, attributes)
         end
       end
     end
@@ -177,9 +183,9 @@ Synvert::Rewriter.new 'rails', 'convert_active_record_dirty_5_0_to_5_1' do
     end
   end
 
-  within_files 'app/{models,observers}/**/*.rb' do
+  within_files 'app/models/**/*.rb' do
     within_node type: 'class' do
-      object_name = node.name.to_source.sub(/Observer$/, '').underscore.tr('/', '_').tableize
+      object_name = node.name.to_source.underscore.tr('/', '_').tableize
 
       with_node type: 'send', receiver: 'self', message: 'table_name=' do
         if node.arguments[0].type == :str
@@ -189,6 +195,16 @@ Synvert::Rewriter.new 'rails', 'convert_active_record_dirty_5_0_to_5_1' do
 
       find_callbacks_and_convert(BEFORE_CALLBACK_NAMES, BEFORE_CALLBACK_CHANGES, object_attributes[object_name])
       find_callbacks_and_convert(AFTER_CALLBACK_NAMES, AFTER_CALLBACK_CHANGES, object_attributes[object_name])
+    end
+  end
+
+  within_files 'app/observers/**/*.rb' do
+    within_node type: 'class' do
+      object_name = node.name.to_source.sub(/Observer$/, '').underscore.tr('/', '_').tableize
+
+      AFTER_CALLBACK_CHANGES.each do |before_name, after_name|
+        convert_send_dirty_api_change(before_name, after_name, object_attributes[object_name])
+      end
     end
   end
 end
