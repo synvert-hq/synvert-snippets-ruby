06454ae746530ac44aa344024e15054a45823d9d
Richard Huang <flyerhzm@gmail.com>
2021-07-18 19:25:45 +0800

    convert octopus to rails 6 multi databases.

---
+++ spec/octopus/convert_to_rails_6_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Convert to rails 6' do
+  let(:rewriter_name) { 'octopus/convert_to_rails_6' }
+  let(:fake_file_path) { 'app/models/user.rb' }
+  let(:test_content) { <<~EOS }
+    messages = self.using(:slave).messages
+  EOS
+  let(:test_rewritten_content) { <<~EOS }
+    messages = ActiveRecord::Base.connected_to(role: :reading) do
+      self.messages
+    end
+  EOS
+
+  include_examples 'convertable'
+end
