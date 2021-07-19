b4256a27b32710a9493c300b770932a5d0166880
Awesome Code <team@xinminlabs.com>
2021-07-19 13:32:22 +0000

    Auto corrected by following Format Ruby Code

--- lib/rails/convert_models_2_3_to_3_0.rb
+++ lib/rails/convert_models_2_3_to_3_0.rb
@@ -331,14 +331,16 @@ Synvert::Rewriter.new 'rails', 'convert_models_2_3_to_3_0' do
     # Post.where("title = \'test\'").update_all("title = \'title\'")
     # Post.where("title = ?", title).update_all("title = \'title\'")
     within_node type: 'send', message: :update_all, arguments: { size: 2 } do
-      replace_with add_receiver_if_necessary("where({{arguments.first}}).update_all({{arguments.last}})")
+      replace_with add_receiver_if_necessary('where({{arguments.first}}).update_all({{arguments.last}})')
     end
 
     # Post.update_all({:title => "title"}, {:title => "test"}, {:limit => 2})
     # =>
     # Post.where(:title => "test").limit(2).update_all(:title => "title")
     within_node type: 'send', message: :update_all, arguments: { size: 3 } do
-      replace_with add_receiver_if_necessary("where({{arguments.first}}).#{generate_new_queries(node.arguments.last)}.update_all({{arguments.second}})")
+      replace_with add_receiver_if_necessary(
+                     "where({{arguments.first}}).#{generate_new_queries(node.arguments.last)}.update_all({{arguments.second}})"
+                   )
     end
 
     # Post.delete_all("title = \'test\'")
