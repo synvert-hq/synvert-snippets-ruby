fe7445e16caa36cdf89c22dab55c05a83691eff0
Dewayne VanHoozer <dvanhoozer@gmail.com>
2021-08-20 14:05:06 -0500

    synvert-snippets Removing debug_me calls
    debug_me  A tool to print the labeled value of variables.
              |__ http://github.com/MadBomber/debug_me
    
    Lets resolve not to keep debugging statement in a
    production code base.  This is my gem.  I use it all
    the time ... and yes, occassionally I have left debug_me
    calls in my commits.  BUT NOW, with synvert I can
    configure a pre-commit hook to remove those calls.

---
+++ lib/ruby/remove_debug_me_calls.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+Synvert::Rewriter.new 'ruby', 'remove_debug_me_calls' do
+  description <<~EOS
+    It removes `debug_me` calls.
+
+    debug_me  A tool to print the labeled value of variables.
+              |__ http://github.com/MadBomber/debug_me
+  EOS
+
+  within_files '**/*.rb' do
+    # removes debug_me methods
+    # removes all debug_me calls that have a block
+    with_node type: 'block', caller: { type: 'send', message: 'debug_me'} do
+      remove
+    end
+
+    # removes all debug_me calls that DO NOT have a block
+    with_node type: 'send', message: 'debug_me' do
+      remove
+    end
+  end
+end
