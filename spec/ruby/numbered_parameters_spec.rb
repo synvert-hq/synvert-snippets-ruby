29d698e45616b88bb7d9c0d9f32ce7fb89cdf7c1
Richard Huang <flyerhzm@gmail.com>
2021-09-14 21:24:41 +0800

    run ruby numbered_parameters test only if ruby version >= 2.7

--- spec/ruby/numbered_parameters_spec.rb
+++ spec/ruby/numbered_parameters_spec.rb
@@ -2,19 +2,21 @@
 
 require 'spec_helper'
 
-RSpec.describe 'Ruby numbered parameters' do
-  let(:rewriter_name) { 'ruby/numbered_parameters' }
-  let(:test_content) { <<~EOS }
-    squared_numbers = (1...10).map { |num| num ** 2 }
+if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.7')
+  RSpec.describe 'Ruby numbered parameters' do
+    let(:rewriter_name) { 'ruby/numbered_parameters' }
+    let(:test_content) { <<~EOS }
+      squared_numbers = (1...10).map { |num| num ** 2 }
 
-    city_populations.each { |city, population| puts "Population of \#{city} is \#{population}" }
-  EOS
+      city_populations.each { |city, population| puts "Population of \#{city} is \#{population}" }
+    EOS
 
-  let(:test_rewritten_content) { <<~EOS }
-    squared_numbers = (1...10).map { _1 ** 2 }
+    let(:test_rewritten_content) { <<~EOS }
+      squared_numbers = (1...10).map { _1 ** 2 }
 
-    city_populations.each { puts "Population of \#{_1} is \#{_2}" }
-  EOS
+      city_populations.each { puts "Population of \#{_1} is \#{_2}" }
+    EOS
 
-  include_examples 'convertable'
+    include_examples 'convertable'
+  end
 end
