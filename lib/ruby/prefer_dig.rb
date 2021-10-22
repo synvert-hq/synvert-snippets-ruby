b051902bdffb423d22370a96866ebe2038703959
Richard Huang <flyerhzm@gmail.com>
2021-10-23 07:13:15 +0800

    check condition in while

--- lib/ruby/prefer_dig.rb
+++ lib/ruby/prefer_dig.rb
@@ -19,11 +19,9 @@ Synvert::Rewriter.new 'ruby', 'prefer_dig' do
     with_node({ type: 'and', right_value: { type: 'send', message: :[], arguments: { size: 1 } } }, { recursive: false }) do
       writeable_node = node.dup
       param_names = []
-      while :and == writeable_node.type && :and == writeable_node.left_value.type
-        if writeable_node.right_value.receiver == writeable_node.left_value.right_value
-          param_names << writeable_node.right_value.arguments.first.to_source
-          writeable_node = writeable_node.left_value
-        end
+      while :and == writeable_node.type && :and == writeable_node.left_value.type && writeable_node.right_value.receiver == writeable_node.left_value.right_value
+        param_names << writeable_node.right_value.arguments.first.to_source
+        writeable_node = writeable_node.left_value
       end
       if :and == writeable_node.type && :send == writeable_node.left_value.type && writeable_node.right_value.receiver == writeable_node.left_value &&
         :[] == writeable_node.left_value.message && writeable_node.left_value.arguments.size == 1
