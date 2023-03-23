825efd92099dd999fa3ec2ebbf4d76f003899140
Richard Huang <flyerhzm@gmail.com>
2023-03-23 14:29:42 +0800

    update rails/convert_to_response_parsed_body snippet

--- lib/rails/convert_to_response_parsed_body.rb
+++ lib/rails/convert_to_response_parsed_body.rb
@@ -8,24 +8,14 @@ Synvert::Rewriter.new 'rails', 'convert_to_response_parsed_body' do
   if_gem 'rails', '>= 5.0'
 
   within_files Synvert::RAILS_CONTROLLER_TEST_FILES do
-    with_node type: 'send',
+    with_node node_type: 'send',
               receiver: 'JSON',
               message: 'parse',
               arguments: {
                 size: 1,
-                first: { type: 'send', receiver: '@response', message: 'body', arguments: { size: 0 } }
+                '0': { node_type: 'send', receiver: { in: ['response', '@response'] }, message: 'body', arguments: { size: 0 } }
               } do
-      replace_with '@response.parsed_body'
-    end
-
-    with_node type: 'send',
-              receiver: 'JSON',
-              message: 'parse',
-              arguments: {
-                size: 1,
-                first: { type: 'send', receiver: 'response', message: 'body', arguments: { size: 0 } }
-              } do
-      replace_with 'response.parsed_body'
+      replace_with '{{arguments.0.receiver}}.parsed_body'
     end
   end
 end
