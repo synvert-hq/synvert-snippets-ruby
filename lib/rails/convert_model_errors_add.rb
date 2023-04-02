8dd09e033828d52cd81fe837d9e57085c8b1f11b
Awesome Code <team@xinminlabs.com>
2023-04-02 12:10:30 +0000

    Auto corrected by following Format Ruby Code

--- lib/rails/convert_model_errors_add.rb
+++ lib/rails/convert_model_errors_add.rb
@@ -20,7 +20,10 @@ Synvert::Rewriter.new 'rails', 'convert_model_errors_add' do
   if_gem 'activerecord', '>= 5.0'
 
   within_files Synvert::RAILS_MODEL_FILES do
-    with_node node_type: 'send', receiver: { node_type: 'send', message: 'errors', arguments: { size: 0 } }, message: '[]=', arguments: { size: 2 } do
+    with_node node_type: 'send',
+              receiver: { node_type: 'send', message: 'errors', arguments: { size: 0 } },
+              message: '[]=',
+              arguments: { size: 2 } do
       replace_with '{{receiver}}.add({{arguments.0}}, {{arguments.1}})'
     end
   end
