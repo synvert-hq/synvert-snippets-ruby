4ad399f46465fd7529e1cf19878550b1f382fcbd
Richard Huang <flyerhzm@gmail.com>
2023-06-01 13:44:36 +0800

    add rails/deprecate_errors_as_hash snippet

---
+++ lib/rails/deprecate_errors_as_hash.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+Synvert::Rewriter.new 'rails', 'deprecate_errors_as_hash' do
+  configure(parser: Synvert::PARSER_PARSER)
+
+  description <<~EOS
+    It deprecates errors as hash.
+
+    ```ruby
+    book.errors[:title] << 'is not interesting enough.'
+    book.errors.values
+    ```
+
+    =>
+
+    ```ruby
+    book.errors.add(:title, 'is not interesting enough.')
+    book.errors.map(&:full_message)
+    ```
+  EOS
+
+  if_gem 'activemodel', '>= 6.1'
+
+  within_files Synvert::ALL_RUBY_FILES do
+    with_node node_type: 'send',
+              receiver: {
+                node_type: 'send',
+                receiver: { node_type: 'send', message: 'errors', arguments: { size: 0 } },
+                message: '[]',
+                arguments: { size: 1 }
+              },
+              message: '<<', arguments: { size: 1 } do
+      replace_with '{{receiver.receiver}}.add({{receiver.arguments.0}}, {{arguments.0}})'
+    end
+
+    with_node node_type: 'send',
+              receiver: { node_type: 'send', message: 'errors', arguments: { size: 0 } },
+              message: 'values',
+              arguments: { size: 0 } do
+      replace_with '{{receiver}}.map(&:message)'
+    end
+  end
+end
---
+++ spec/rails/deprecate_errors_as_hash_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Deprecate errors as hash' do
+  let(:rewriter_name) { 'rails/deprecate_errors_as_hash' }
+  let(:fake_file_path) { 'app/controllers/posts_controller.rb' }
+  let(:test_content) { <<~EOS }
+    class PostsController < ApplicationController
+      def create
+        @post = Post.create(post_params)
+        @post.errors[:title] << 'is not interesting enough.'
+        render json: { errors: @post.errors.values }
+      end
+    end
+  EOS
+
+  let(:test_rewritten_content) { <<~EOS }
+    class PostsController < ApplicationController
+      def create
+        @post = Post.create(post_params)
+        @post.errors.add(:title, 'is not interesting enough.')
+        render json: { errors: @post.errors.map(&:message) }
+      end
+    end
+  EOS
+
+  include_examples 'convertable'
+end
