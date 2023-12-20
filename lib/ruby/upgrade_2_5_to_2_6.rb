597069bc420c3a61b8c0e64bfc8c3492d2799947
Richard Huang <flyerhzm@gmail.com>
2023-12-20 14:43:29 +0800

    update all upgrade ruby snippets

--- lib/ruby/upgrade_2_5_to_2_6.rb
+++ lib/ruby/upgrade_2_5_to_2_6.rb
@@ -5,8 +5,8 @@ Synvert::Rewriter.new 'ruby', 'upgrade_2_5_to_2_6' do
     It upgrades ruby 2.5 to 2.6
   EOS
 
+  add_snippet 'ruby', 'deprecate_big_decimal_new'
   add_snippet 'ruby', 'deprecate_dir_exists'
   add_snippet 'ruby', 'deprecate_file_exists'
   add_snippet 'ruby', 'deprecate_fixnum_and_bignum'
-  add_snippet 'ruby', 'deprecate_big_decimal_new'
 end
--- lib/ruby/upgrade_2_6_to_2_7.rb
+++ lib/ruby/upgrade_2_6_to_2_7.rb
@@ -5,6 +5,7 @@ Synvert::Rewriter.new 'ruby', 'upgrade_2_6_to_2_7' do
     It upgrades ruby 2.6 to 2.7
   EOS
 
+  add_snippet 'ruby', 'deprecate_big_decimal_new'
   add_snippet 'ruby', 'deprecate_dir_exists'
   add_snippet 'ruby', 'deprecate_file_exists'
   add_snippet 'ruby', 'deprecate_fixnum_and_bignum'
