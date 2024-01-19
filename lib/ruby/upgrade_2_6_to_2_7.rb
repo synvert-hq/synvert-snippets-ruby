2a2ec1752b7e1a9332787bce49da4c326d7cb731
Richard Huang <flyerhzm@gmail.com>
2024-01-19 18:28:16 +0800

    remove ruby/numbered_parameters from ruby/upgrade_2_6_to_2_7

--- lib/ruby/upgrade_2_6_to_2_7.rb
+++ lib/ruby/upgrade_2_6_to_2_7.rb
@@ -10,6 +10,5 @@ Synvert::Rewriter.new 'ruby', 'upgrade_2_6_to_2_7' do
   add_snippet 'ruby', 'deprecate_file_exists'
   add_snippet 'ruby', 'deprecate_fixnum_and_bignum'
   add_snippet 'ruby', 'kernel_open_to_uri_open'
-  add_snippet 'ruby', 'numbered_parameters'
   add_snippet 'ruby', 'uri_escape_to_uri_default_parser_escape'
 end
