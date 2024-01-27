02549edc5deb4e4b80b9299e49a4c72eb0a659ac
Richard Huang <flyerhzm@gmail.com>
2024-01-27 23:11:18 +0800

    rewrite ruby/nested_class_definition snippet

--- lib/ruby/nested_class_definition.rb
+++ lib/ruby/nested_class_definition.rb
@@ -23,40 +23,25 @@ Synvert::Rewriter.new 'ruby', 'nested_class_definition' do
     ```
   EOS
 
-  redo_until_no_change
-
-  original_names = []
-
-  # round one
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
     # class Foo::Bar < Base
     #   def test; end
     # end
     # =>
     # module Foo
-    #   class Foo::Bar < Base
+    #   class Bar < Base
     #     def test; end
     #   end
     # end
     find_node '.class[name=~/::/]' do
-      original_name = node.name.to_source
-      original_names << original_name
-      module_name, = original_name.split('::', 2)
-      wrap prefix: "module #{module_name}", suffix: 'end', newline: true
-    end
-  end
+      source = node.to_source
+      parts = node.name.to_source.split('::')
+      class_name = parts.pop
+      new_parts = parts.map.with_index { |mod, index| ' ' * NodeMutation.tab_width * index + "module #{mod}" }
+      new_parts.concat(source.sub(parts.join('::') + '::', '').split("\n").map { |line| (' ' * NodeMutation.tab_width * parts.size) + line })
+      new_parts.concat(parts.map.with_index { |mod, index| ' ' * NodeMutation.tab_width * (parts.size - index - 1) + "end" })
 
-  # round two
-  within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
-    # class Foo:Bar < Base
-    # end
-    # =>
-    # class Bar < Base
-    # end
-    find_node ".class[name IN (#{original_names.join(' ')})]" do
-      original_name = node.name.to_source
-      _, class_name = original_name.split('::', 2)
-      replace :name, with: class_name
+      replace_with new_parts.join("\n")
     end
   end
 end
