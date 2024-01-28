09589fbff9835f40abaa21d74115972999de0f41
Awesome Code <team@xinminlabs.com>
2024-01-28 08:17:21 +0000

    Auto corrected by following Format Ruby Code

--- lib/rails/convert_views_3_2_to_4_0.rb
+++ lib/rails/convert_views_3_2_to_4_0.rb
@@ -23,14 +23,30 @@ Synvert::Rewriter.new 'rails', 'convert_views_3_2_to_4_0' do
     # link_to 'delete', post_path(post), confirm: 'Are you sure to delete post?'
     # =>
     # link_to 'delete', post_path(post), data: { confirm: 'Are you sure to delete post?' }
-    within_node node_type: 'send', message: 'link_to', arguments: { last: { node_type: 'hash', confirm_value: { not: nil }, data_value: nil } } do
+    within_node node_type: 'send',
+                message: 'link_to',
+                arguments: {
+                  last: {
+                    node_type: 'hash',
+                    confirm_value: { not: nil },
+                    data_value: nil
+                  }
+                } do
       group do
         delete 'arguments.last.confirm_pair', and_comma: true
         insert 'data: { confirm: {{arguments.last.confirm_value}} }', to: 'arguments.last', and_comma: true
       end
     end
 
-    within_node node_type: 'send', message: 'link_to', arguments: { last: { node_type: 'hash', confirm_value: { not: nil }, data_value: { not: nil } } } do
+    within_node node_type: 'send',
+                message: 'link_to',
+                arguments: {
+                  last: {
+                    node_type: 'hash',
+                    confirm_value: { not: nil },
+                    data_value: { not: nil }
+                  }
+                } do
       group do
         delete 'arguments.last.confirm_pair', and_comma: true
         insert 'confirm: {{arguments.last.confirm_value}}', to: 'arguments.last.data_pair.value.pairs', and_comma: true
