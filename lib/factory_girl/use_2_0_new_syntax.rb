942756a4e98304754dc33fadd994bdab5a6bbc5e
Richard Huang <flyerhzm@gmail.com>
2024-01-28 10:13:43 +0800

    remove factory_girl snippets

--- lib/factory_girl/use_2_0_new_syntax.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Synvert::Rewriter.new 'factory_girl', 'use_2_0_new_syntax' do
-  description 'It uses factory_girl new syntax.'
-
-  add_snippet 'factory_girl', 'use_short_syntax'
-  add_snippet 'factory_girl', 'fix_2_0_deprecations'
-end
