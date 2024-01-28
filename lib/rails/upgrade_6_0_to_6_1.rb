1c02110a685187a49defc5d739bd127454e86d79
Richard Huang <flyerhzm@gmail.com>
2024-01-28 21:56:54 +0800

    update rails/upgrade_6_0_to_6_1

--- lib/rails/upgrade_6_0_to_6_1.rb
+++ lib/rails/upgrade_6_0_to_6_1.rb
@@ -11,4 +11,5 @@ Synvert::Rewriter.new 'rails', 'upgrade_6_0_to_6_1' do
   add_snippet 'rails', 'convert_update_attributes_to_update'
   add_snippet 'rails', 'deprecate_errors_as_hash'
   add_snippet 'rails', 'rename_errors_keys_to_attribute_names'
+  add_snippet 'rails', 'use_active_storage_image_processing_macros'
 end
