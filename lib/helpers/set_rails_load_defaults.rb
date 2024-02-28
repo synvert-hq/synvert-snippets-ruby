d6b825703c3a2f6e17a454c4a4d589b2c27ce6db
Richard Huang <flyerhzm@gmail.com>
2024-02-28 13:11:36 +0800

    use prism adapter for helpers/set_rails_load_defaults

--- lib/helpers/set_rails_load_defaults.rb
+++ lib/helpers/set_rails_load_defaults.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 Synvert::Helper.new 'rails/set_load_defaults' do |options|
+  configure(parser: Synvert::PRISM_PARSER)
+
   rails_version = options[:rails_version]
 
   within_file 'config/application.rb' do
-    with_node node_type: 'class', name: 'Application' do
+    with_node node_type: 'class_node', constant_path: 'Application' do
       exists = false
-      with_node node_type: 'send', receiver: 'config', message: 'load_defaults', arguments: { size: 1 } do
+      with_node node_type: 'call_node', receiver: 'config', name: 'load_defaults', arguments: { arguments: { length: 1 } } do
         exists = true
         replace_with "config.load_defaults #{rails_version}"
       end
