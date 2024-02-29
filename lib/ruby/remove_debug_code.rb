349b8a657a7078917b693659000245e61bf07060
Richard Huang <flyerhzm@gmail.com>
2024-02-29 21:15:33 +0800

    use prism adapter for ruby/remove_debug_code

--- lib/ruby/remove_debug_code.rb
+++ lib/ruby/remove_debug_code.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'ruby', 'remove_debug_code' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It removes `puts` and `p` calls.
@@ -9,7 +9,7 @@ Synvert::Rewriter.new 'ruby', 'remove_debug_code' do
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
     # removes puts and p methods
-    find_node '.send[receiver=nil][message IN (puts p)]' do
+    find_node '.call_node[receiver=nil][name IN (puts p)]' do
       remove
     end
   end
