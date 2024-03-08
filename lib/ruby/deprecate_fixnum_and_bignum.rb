3719c88995e0e9d115cf95b9b005989d5c2b16ac
Richard Huang <flyerhzm@gmail.com>
2024-03-08 15:45:11 +0800

    use prism adapter for ruby/deprecate_fixnum_and_bignum

--- lib/ruby/deprecate_fixnum_and_bignum.rb
+++ lib/ruby/deprecate_fixnum_and_bignum.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'ruby', 'deprecate_fixnum_and_bignum' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     Unify Fixnum and Bignum into Integer.
@@ -10,7 +10,7 @@ Synvert::Rewriter.new 'ruby', 'deprecate_fixnum_and_bignum' do
   if_ruby '2.4.0'
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
-    find_node '.const[name IN (Fixnum Bignum)]' do
+    find_node '.constant_read_node[name IN (Fixnum Bignum)]' do
       replace_with 'Integer'
     end
   end
