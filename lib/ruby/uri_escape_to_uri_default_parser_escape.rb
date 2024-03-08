6036a7727a3f79e4ff580a8ff02c32663bfb6c45
Richard Huang <flyerhzm@gmail.com>
2024-03-08 15:52:58 +0800

    use prism adapter for ruby/uri_escape_to_uri_default_parser_escape

--- lib/ruby/uri_escape_to_uri_default_parser_escape.rb
+++ lib/ruby/uri_escape_to_uri_default_parser_escape.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'ruby', 'uri_escape_to_uri_default_parser_escape' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     Use URI::DEFAULT_PARSER.escape instead of URI.escape.
@@ -23,14 +23,10 @@ Synvert::Rewriter.new 'ruby', 'uri_escape_to_uri_default_parser_escape' do
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
     # URI.escape(url) => URI::DEFAULT_PARSER.escape(url)
-    find_node '.send[receiver=URI][message=escape]' do
-      replace :receiver, with: 'URI::DEFAULT_PARSER'
-    end
-
     # URI.encode(url) => URI::DEFAULT_PARSER.escape(url)
-    find_node '.send[receiver=URI][message=encode]' do
+    find_node '.call_node[receiver=URI][name IN (escape encode)][arguments=.arguments_node[arguments.size=1]]' do
       group do
-        replace :receiver, with: 'URI::DEFAULT_PARSER'
+        replace :receiver, with: '{{receiver}}::DEFAULT_PARSER'
         replace :message, with: 'escape'
       end
     end
