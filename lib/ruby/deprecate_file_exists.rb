911231f09a6d38b2a81c731d2d36b6b7830a52ef
Richard Huang <flyerhzm@gmail.com>
2024-03-08 15:47:28 +0800

    use prism adapter for ruby/deprecate_file_exists

--- lib/ruby/deprecate_file_exists.rb
+++ lib/ruby/deprecate_file_exists.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'ruby', 'deprecate_file_exists' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     `File.exists?` is deprecated in Ruby 2.1.0, and removed in Ruby 3.2.0.
@@ -12,8 +12,8 @@ Synvert::Rewriter.new 'ruby', 'deprecate_file_exists' do
   if_ruby '2.1.0'
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
-    find_node '.send[receiver=File][message=exists?]' do
-      replace :message, with: 'exist?'
+    find_node '.call_node[receiver=File][name=exists?][arguments=.arguments_node[arguments.size=1]]' do
+      replace :name, with: 'exist?'
     end
   end
 end
