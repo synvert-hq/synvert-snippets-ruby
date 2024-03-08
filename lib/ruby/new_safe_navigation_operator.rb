4c8ea4833baffea86adf1db3f42694413f075318
Richard Huang <flyerhzm@gmail.com>
2024-03-08 16:10:39 +0800

    use prism adapter for ruby/new_safe_navigation_operator

--- lib/ruby/new_safe_navigation_operator.rb
+++ lib/ruby/new_safe_navigation_operator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'ruby', 'new_safe_navigation_operator' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     Use ruby new safe navigation operator.
@@ -25,14 +25,14 @@ Synvert::Rewriter.new 'ruby', 'new_safe_navigation_operator' do
     # =>
     # u&.profile&.thumbnails&.large(100, format: 'jpg')
     # u&.profile&.thumbnails&.large(100, format: 'jpg')
-    find_node '.send[message IN (try try!)][arguments.size > 0]' do
+    find_node '.call_node[name IN (try try!)][arguments != nil][arguments.arguments.size > 0]' do
       group do
         insert '&', to: 'receiver'
-        replace :message, with: '{{arguments.first.to_value}}'
-        if node.arguments.size == 1
-          delete :arguments, :parentheses
+        replace :message, with: '{{arguments.arguments.first.to_value}}'
+        if node.arguments.arguments.size == 1
+          delete :opening, :arguments, :closing
         else
-          delete 'arguments.first', and_comma: true
+          delete 'arguments.arguments.first', and_comma: true
         end
       end
     end
