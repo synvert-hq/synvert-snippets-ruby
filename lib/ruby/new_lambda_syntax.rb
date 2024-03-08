b261965ac4c8d743e2c0701e92069cfb3c613709
Richard Huang <flyerhzm@gmail.com>
2024-03-08 16:54:55 +0800

    use prism adapter for ruby/new_lambda_syntax

--- lib/ruby/new_lambda_syntax.rb
+++ lib/ruby/new_lambda_syntax.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'ruby', 'new_lambda_syntax' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     Use ruby new lambda syntax
@@ -23,13 +23,13 @@ Synvert::Rewriter.new 'ruby', 'new_lambda_syntax' do
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
     # lambda { test } => -> { test }
-    find_node '.block[caller=.send[receiver=nil][message=lambda]][arguments.size=0]' do
-      replace_with '-> { {{body}} }'
+    find_node '.call_node[receiver=nil][name=lambda][block=.block_node][parameters=nil]' do
+      replace_with '-> { {{block.body}} }'
     end
 
     # lambda { |a, b, c| a + b + c } => ->(a, b, c) { a + b + c }
-    find_node '.block[caller=.send[receiver=nil][message=lambda]][arguments.size > 0]' do
-      replace_with '->({{arguments}}) { {{body}} }'
+    find_node '.call_node[receiver=nil][name=lambda][block=.block_node[parameters=.block_parameters_node]]' do
+      replace_with '->({{block.parameters.parameters}}) { {{block.body}} }'
     end
   end
 end
