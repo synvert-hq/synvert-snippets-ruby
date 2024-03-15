2be83e60282bb0f639c2d041bf8c9ca6aec75931
Richard Huang <flyerhzm@gmail.com>
2024-03-15 17:14:58 +0800

    use PRISM_PARSER in rails/convert_configs_6_1_to_7_0

--- lib/rails/convert_configs_6_1_to_7_0.rb
+++ lib/rails/convert_configs_6_1_to_7_0.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'convert_configs_6_1_to_7_0' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It converts rails configs from 6.1 to 7.0
 
     It sets `config.load_defaults 7.0` in config/application.rb.
+
+    It removes `config.autoloader = :classic` in config/application.rb.
   EOS
 
   if_gem 'rails', '~> 7.0.0'
@@ -14,7 +16,10 @@ Synvert::Rewriter.new 'rails', 'convert_configs_6_1_to_7_0' do
   call_helper 'rails/set_load_defaults', rails_version: '7.0'
 
   within_files 'config/application.rb' do
-    find_node '.send[receiver=.send[receiver=nil][message=config][arguments.size=0]][message=autoloader=][arguments.size=1]' do
+    with_node node_type: 'call_node',
+              receiver: { node_type: 'call_node', name: 'config' },
+              name: 'autoloader=',
+              arguments: { node_type: 'arguments_node', arguments: { size: { gt: 0 } } } do
       remove
     end
   end
