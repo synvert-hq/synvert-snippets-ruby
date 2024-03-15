a6bdfdd4a44b192666702f1546e26363a87d01a8
Richard Huang <flyerhzm@gmail.com>
2024-03-15 10:05:45 +0800

    use prism adapter for rails/convert_configs_3_1_to_3_2

--- lib/rails/convert_configs_3_1_to_3_2.rb
+++ lib/rails/convert_configs_3_1_to_3_2.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'convert_configs_3_1_to_3_2' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It upgrades rails configs from 3.1 to 3.2.
@@ -24,32 +24,26 @@ Synvert::Rewriter.new 'rails', 'convert_configs_3_1_to_3_2' do
 
   within_file 'config/environments/development.rb' do
     # prepend config.active_record.auto_explain_threshold_in_seconds = 0.5
-    unless_exist_node node_type: 'send',
+    unless_exist_node node_type: 'call_node',
                       receiver: {
-                        node_type: 'send',
-                        receiver: {
-                          node_type: 'send',
-                          message: 'config'
-                        },
-                        message: 'active_record'
+                        node_type: 'call_node',
+                        receiver: 'config',
+                        name: 'active_record'
                       },
-                      message: 'auto_explain_threshold_in_seconds=' do
+                      name: 'auto_explain_threshold_in_seconds=' do
       prepend 'config.active_record.auto_explain_threshold_in_seconds = 0.5'
     end
   end
 
   within_files 'config/environments/{development,test}.rb' do
     # prepend config.active_record.mass_assignment_sanitizer = :strict
-    unless_exist_node node_type: 'send',
+    unless_exist_node node_type: 'call_node',
                       receiver: {
-                        node_type: 'send',
-                        receiver: {
-                          node_type: 'send',
-                          message: 'config'
-                        },
-                        message: 'active_record'
+                        node_type: 'call_node',
+                        receiver: 'config',
+                        name: 'active_record'
                       },
-                      message: 'mass_assignment_sanitizer=' do
+                      name: 'mass_assignment_sanitizer=' do
       prepend 'config.active_record.mass_assignment_sanitizer = :strict'
     end
   end
