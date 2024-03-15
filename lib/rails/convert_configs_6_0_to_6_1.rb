b70a53aba82426bc3e043f0a811350a5fe397f88
Richard Huang <flyerhzm@gmail.com>
2024-03-15 17:12:06 +0800

    use PRISM_PARSER in rails/convert_configs_6_0_to_6_1

--- lib/rails/convert_configs_6_0_to_6_1.rb
+++ lib/rails/convert_configs_6_0_to_6_1.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'convert_configs_6_0_to_6_1' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It converts rails configs from 6.0 to 6.1
