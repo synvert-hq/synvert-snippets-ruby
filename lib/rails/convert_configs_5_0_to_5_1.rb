f056831e9518440920d2adfbc8d9f6f17de59241
Richard Huang <flyerhzm@gmail.com>
2024-03-15 17:10:33 +0800

    use PRISM_PARSER in rails/convert_configs_5_0_to_5_1

--- lib/rails/convert_configs_5_0_to_5_1.rb
+++ lib/rails/convert_configs_5_0_to_5_1.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'convert_configs_5_0_to_5_1' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It converts rails configs from 5.0 to 5.1.
