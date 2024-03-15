a3a42fcd46a948abd6e0ae1fab2ec91aa6587f7b
Richard Huang <flyerhzm@gmail.com>
2024-03-15 16:08:25 +0800

    no need to use if_only_exist_node

--- lib/rspec/one_liner_expectation.rb
+++ lib/rspec/one_liner_expectation.rb
@@ -38,13 +38,11 @@ Synvert::Rewriter.new 'rspec', 'one_liner_expectation' do
     { should: 'to', should_not: 'not_to' }.each do |old_message, new_message|
       # it { should matcher } => it { is_expected.to matcher }
       # it { should_not matcher } => it { is_expected.not_to matcher }
-      with_node node_type: 'block', caller: { message: 'it' } do
-        if_only_exist_node node_type: 'send', receiver: nil, message: old_message do
-          receiver = node.body.first.arguments.first.receiver
-          unless receiver && matcher_converters.include?(receiver.message)
-            matcher = node.body.first.arguments.first.to_source
-            replace_with "it { is_expected.#{new_message} #{matcher} }"
-          end
+      with_node node_type: 'block', caller: { message: 'it' }, body: { size: 1, first: { node_type: 'send', receiver: nil, message: old_message } } do
+        receiver = node.body.first.arguments.first.receiver
+        unless receiver && matcher_converters.include?(receiver.message)
+          matcher = node.body.first.arguments.first.to_source
+          replace_with "it { is_expected.#{new_message} #{matcher} }"
         end
       end
 
@@ -60,34 +58,36 @@ Synvert::Rewriter.new 'rspec', 'one_liner_expectation' do
       #   expect(subject.players.size).to be >= 3
       # end
       matcher_converters.each do |old_matcher, new_matcher|
-        with_node node_type: 'block', caller: { message: 'it' } do
-          if_only_exist_node node_type: 'send',
-                             receiver: nil,
-                             message: old_message,
-                             arguments: {
-                               first: {
-                                 node_type: 'send',
-                                 receiver: {
-                                   node_type: 'send',
-                                   message: old_matcher
-                                 }
-                               }
-                             } do
-            times = node.body.first.arguments.first.receiver.arguments.first.to_source
-            items_name = node.body.first.arguments.first.message
-            if :items == items_name
-              replace_with <<~EOS
-                it 'has #{times} items' do
-                  expect(subject.size).#{new_message} #{new_matcher}(#{times})
-                end
-              EOS
-            else
-              replace_with <<~EOS
-                it '#{old_matcher.to_s.sub('have', 'has').tr('_', ' ')} #{times} #{items_name}' do
-                  expect(subject.#{items_name}.size).#{new_message} #{new_matcher} #{times}
-                end
-              EOS
-            end
+        with_node node_type: 'block',
+                  caller: { message: 'it' },
+                  body: { size: 1, first: {
+                    node_type: 'send',
+                    receiver: nil,
+                    message: old_message,
+                    arguments: {
+                      first: {
+                        node_type: 'send',
+                        receiver: {
+                          node_type: 'send',
+                          message: old_matcher
+                        }
+                      }
+                    }
+                  } } do
+          times = node.body.first.arguments.first.receiver.arguments.first.to_source
+          items_name = node.body.first.arguments.first.message
+          if :items == items_name
+            replace_with <<~EOS
+              it 'has #{times} items' do
+                expect(subject.size).#{new_message} #{new_matcher}(#{times})
+              end
+            EOS
+          else
+            replace_with <<~EOS
+              it '#{old_matcher.to_s.sub('have', 'has').tr('_', ' ')} #{times} #{items_name}' do
+                expect(subject.#{items_name}.size).#{new_message} #{new_matcher} #{times}
+              end
+            EOS
           end
         end
       end
