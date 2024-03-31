35c0f8c1e2d29d54663fd43046ff795b55dd9d5c
Richard Huang <flyerhzm@gmail.com>
2024-03-31 18:54:57 +0800

    use PRISM_PARSER in rails/convert_after_commit

--- lib/rails/convert_after_commit.rb
+++ lib/rails/convert_after_commit.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'convert_after_commit' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It converts active_record after_commit.
@@ -38,16 +38,13 @@ Synvert::Rewriter.new 'rails', 'convert_after_commit' do
     # after_create_commit :add_to_index_later
     # after_update_commit :update_in_index_later
     # after_destroy_commit :remove_from_index_later
-    with_node node_type: 'send',
+    with_node node_type: 'call_node',
               receiver: nil,
-              message: 'after_commit',
-              arguments: {
-                size: 2,
-                '1': { node_type: 'hash', on_value: { in: %i[create update destroy] } }
-              } do
+              name: 'after_commit',
+              arguments: { node_type: 'arguments_node', arguments: { size: 2, '1': { node_type: 'keyword_hash_node', on_value: { in: %i[create update destroy] } } } } do
       group do
-        replace :message, with: 'after_{{arguments.-1.on_value.to_value}}_commit'
-        delete 'arguments.-1.on_pair', and_comma: true
+        replace :name, with: 'after_{{arguments.arguments.-1.on_value.to_value}}_commit'
+        delete 'arguments.arguments.-1.on_element', and_comma: true
       end
     end
 
@@ -60,21 +57,18 @@ Synvert::Rewriter.new 'rails', 'convert_after_commit' do
     # after_update_commit :update_in_index_later
     # after_save_commit :save_to_index_later
     # after_destroy_commit :remove_from_index_later
-    with_node node_type: 'send',
+    with_node node_type: 'call_node',
               receiver: nil,
               message: 'after_commit',
-              arguments: {
-                size: 2,
-                '1': { node_type: 'hash', on_value: { node_type: 'array' } }
-              } do
+              arguments: { node_type: 'arguments_node', arguments: { size: 2, '1': { node_type: 'keyword_hash_node', on_value: { node_type: 'array_node' } } } } do
       group do
-        if node.arguments[1].on_value.elements.size == 1
-          replace :message, with: 'after_{{arguments.-1.on_value.elements.0.to_value}}_commit'
-          delete 'arguments.-1.on_pair', and_comma: true
-        elsif node.arguments[1].on_value.elements.size == 2
-          if (node.arguments[1].on_value.elements.map(&:to_value) & %i[create update]).size == 2
+        if node.arguments.arguments[1].on_value.elements.size == 1
+          replace :message, with: 'after_{{arguments.arguments.-1.on_value.elements.0.to_value}}_commit'
+          delete 'arguments.arguments.-1.on_element', and_comma: true
+        elsif node.arguments.arguments[1].on_value.elements.size == 2
+          if (node.arguments.arguments[1].on_value.elements.map(&:to_value) & %i[create update]).size == 2
             replace :message, with: 'after_save_commit'
-            delete 'arguments.-1.on_pair', and_comma: true
+            delete 'arguments.arguments.-1.on_element', and_comma: true
           end
         end
       end
