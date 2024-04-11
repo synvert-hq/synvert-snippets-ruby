243d74b5eb9ab669902f0d7fa3ddcab363fe3d7b
Richard Huang <flyerhzm@gmail.com>
2024-04-11 19:19:12 +0800

    use PRISM_PARSER in rails/use_active_storage_image_processing_macros

--- lib/rails/use_active_storage_image_processing_macros.rb
+++ lib/rails/use_active_storage_image_processing_macros.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'use_active_storage_image_processing_macros' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It uses active_storage image processing macros.
@@ -24,15 +24,15 @@ Synvert::Rewriter.new 'rails', 'use_active_storage_image_processing_macros' do
   if_gem 'activestorage', '>= 6.1'
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
-    with_node node_type: 'send', message: 'preview', arguments: { size: 1, first: { node_type: 'hash', resize_value: { not: nil } } } do
-      resize_value = node.arguments.first.resize_value.to_value
+    with_node node_type: 'call_node', name: 'preview', arguments: { node_type: 'arguments_node', arguments: { size: 1, first: { node_type: 'keyword_hash_node', resize_value: { not: nil } } } } do
+      resize_value = node.arguments.arguments.first.resize_value.to_value
       width, height = resize_value.split('x')
       if resize_value.ends_with?('>')
-        replace 'arguments.first.resize_pair', with: "resize_to_limit: [#{width.to_i}, #{height.to_i}]"
+        replace 'arguments.arguments.first.resize_element', with: "resize_to_limit: [#{width.to_i}, #{height.to_i}]"
       elsif resize_value.ends_with?('^')
-        replace 'arguments.first.resize_pair', with: "resize_to_fill: [#{width.to_i}, #{height.to_i}]"
+        replace 'arguments.arguments.first.resize_element', with: "resize_to_fill: [#{width.to_i}, #{height.to_i}]"
       else
-        replace 'arguments.first.resize_pair', with: "resize_to_fit: [#{width.to_i}, #{height.to_i}]"
+        replace 'arguments.arguments.first.resize_element', with: "resize_to_fit: [#{width.to_i}, #{height.to_i}]"
       end
     end
   end
