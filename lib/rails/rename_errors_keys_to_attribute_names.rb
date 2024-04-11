c5d7a6f3953f4700623d06487ed5939277841269
Richard Huang <flyerhzm@gmail.com>
2024-04-11 20:13:23 +0800

    use PRISM_PARSER in rails/rename_errors_keys_to_attribute_names

--- lib/rails/rename_errors_keys_to_attribute_names.rb
+++ lib/rails/rename_errors_keys_to_attribute_names.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'rename_errors_keys_to_attribute_names' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It renames `errors#keys` to `erros#attribute_names`
@@ -10,10 +10,10 @@ Synvert::Rewriter.new 'rails', 'rename_errors_keys_to_attribute_names' do
   if_gem 'rails', '>= 6.1'
 
   within_files Synvert::RAILS_MODEL_FILES do
-    with_node node_type: 'send',
-              receiver: { node_type: 'send', message: 'errors', arguments: { size: 0 } },
-              message: 'keys',
-              arguments: { size: 0 } do
+    with_node node_type: 'call_node',
+              receiver: { node_type: 'call_node', name: 'errors', arguments: nil },
+              name: 'keys',
+              arguments: nil do
       replace :message, with: 'attribute_names'
     end
   end
