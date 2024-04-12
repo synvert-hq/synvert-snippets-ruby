07474dbe1f709cc853fa902334d31a189805f5ce
Richard Huang <flyerhzm@gmail.com>
2024-04-12 16:13:51 +0530

    use PRISM_PARSER in rails/convert_views_3_2_to_4_0

--- lib/rails/convert_views_3_2_to_4_0.rb
+++ lib/rails/convert_views_3_2_to_4_0.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'convert_views_3_2_to_4_0' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It converts rails views from 3.2 to 4.0.
@@ -23,17 +23,31 @@ Synvert::Rewriter.new 'rails', 'convert_views_3_2_to_4_0' do
     # link_to 'delete', post_path(post), confirm: 'Are you sure to delete post?'
     # =>
     # link_to 'delete', post_path(post), data: { confirm: 'Are you sure to delete post?' }
-    within_node node_type: 'send', message: 'link_to', arguments: { last: { node_type: 'hash', confirm_value: { not: nil }, data_value: nil } } do
+    within_node node_type: 'call_node',
+                name: 'link_to',
+                arguments: {
+                  node_type: 'arguments_node',
+                  arguments: {
+                    last: { node_type: 'keyword_hash_node', confirm_value: { not: nil }, data_value: nil }
+                  }
+                } do
       group do
-        delete 'arguments.last.confirm_pair', and_comma: true
-        insert 'data: { confirm: {{arguments.last.confirm_value}} }', to: 'arguments.last', and_comma: true
+        delete 'arguments.arguments.last.confirm_element', and_comma: true
+        insert 'data: { confirm: {{arguments.arguments.last.confirm_value}} }', to: 'arguments.arguments.last', and_comma: true
       end
     end
 
-    within_node node_type: 'send', message: 'link_to', arguments: { last: { node_type: 'hash', confirm_value: { not: nil }, data_value: { not: nil } } } do
+    within_node node_type: 'call_node',
+                name: 'link_to',
+                arguments: {
+                  node_type: 'arguments_node',
+                  arguments: {
+                    last: { node_type: 'keyword_hash_node', confirm_value: { not: nil }, data_value: { not: nil } }
+                  }
+                } do
       group do
-        delete 'arguments.last.confirm_pair', and_comma: true
-        insert 'confirm: {{arguments.last.confirm_value}}', to: 'arguments.last.data_pair.value.pairs', and_comma: true
+        delete 'arguments.arguments.last.confirm_element', and_comma: true
+        insert '{{arguments.arguments.last.confirm_element}}', to: 'arguments.arguments.last.data_element.value.elements', and_comma: true
       end
     end
   end
