a33ddc78bbabddd8b6314d5b104b07803c20ef0e
Richard Huang <flyerhzm@gmail.com>
2024-04-12 17:06:38 +0800

    use PRISM_PARSER in rails/prefer_nor_conditions

--- lib/rails/prefer_nor_conditions.rb
+++ lib/rails/prefer_nor_conditions.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'prefer_nor_conditions' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     Prefer NOR conditions
@@ -20,10 +20,13 @@ Synvert::Rewriter.new 'rails', 'prefer_nor_conditions' do
   if_gem 'rails', '>= 6.0'
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
-    find_node '.send[receiver=.send[message=where][arguments.size=0]][message=not][arguments.size=1][arguments.0=.hash[pairs.length>1]]' do
-      new_source = node.arguments[0].pairs.map { |pair| "where.not(#{pair.to_source})" }
+    with_node node_type: 'call_node',
+              receiver: { node_type: 'call_node', name: 'where', arguments: nil },
+              name: 'not',
+              arguments: { node_type: 'arguments_node', arguments: { size: 1, first: { node_type: 'keyword_hash_node', elements: { length: { gt: 1 } } } } } do
+      new_source = node.arguments.arguments.first.elements.map { |element| "where.not(#{element.to_source})" }
                        .join('.')
-      replace 'receiver.message', :message, :parentheses, with: new_source
+      replace 'receiver.message', :closing, with: new_source
     end
   end
 end
