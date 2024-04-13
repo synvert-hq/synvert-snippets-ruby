25343826491a40fbcc9555a54e704253f911d715
Richard Huang <flyerhzm@gmail.com>
2024-04-13 17:28:37 +0800

    use PRISM_PARSER in rails/convert_to_response_parsed_body

--- lib/rails/convert_to_response_parsed_body.rb
+++ lib/rails/convert_to_response_parsed_body.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'convert_to_response_parsed_body' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It converts `JSON.parse(@response.body)` to `@response.parsed_body`.
@@ -10,19 +10,22 @@ Synvert::Rewriter.new 'rails', 'convert_to_response_parsed_body' do
   if_gem 'rails', '>= 5.0'
 
   within_files Synvert::RAILS_CONTROLLER_TEST_FILES do
-    with_node node_type: 'send',
+    with_node node_type: 'call_node',
               receiver: 'JSON',
-              message: 'parse',
+              name: 'parse',
               arguments: {
-                size: 1,
-                '0': {
-                  node_type: 'send',
-                  receiver: { in: ['response', '@response'] },
-                  message: 'body',
-                  arguments: { size: 0 }
+                node_type: 'arguments_node',
+                arguments: {
+                  size: 1,
+                  first: {
+                    node_type: 'call_node',
+                    receiver: { in: ['response', '@response'] },
+                    message: 'body',
+                    arguments: nil
+                  }
                 }
               } do
-      replace_with '{{arguments.0.receiver}}.parsed_body'
+      replace_with '{{arguments.arguments.0.receiver}}.parsed_body'
     end
   end
 end
