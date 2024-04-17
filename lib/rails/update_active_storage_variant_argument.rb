0b588e1e5cbba7a3d370953c5bbad849b0ed6879
Richard Huang <flyerhzm@gmail.com>
2024-04-17 19:22:01 +0800

    use PRISM_PARSER in rails/update_active_storage_variant_argument

--- lib/rails/update_active_storage_variant_argument.rb
+++ lib/rails/update_active_storage_variant_argument.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'update_active_storage_variant_argument' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It updates active_storage variant method argument.
@@ -26,22 +26,30 @@ Synvert::Rewriter.new 'rails', 'update_active_storage_variant_argument' do
   if_gem 'activestorage', '>= 7.0'
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
-    with_node node_type: 'send', message: 'variant', arguments: { size: 1, first: { node_type: 'hash', resize_value: { node_type: 'str', value: { last: 'x' } } } } do
-      width = node.arguments.first.resize_value.to_value.to_i
-      replace 'arguments.first.resize_pair', with: "resize_to_limit: [#{width}, nil]"
+    with_node node_type: 'call_node',
+              name: 'variant',
+              arguments: { node_type: 'arguments_node', arguments: { size: 1, first: { node_type: 'keyword_hash_node', resize_value: { node_type: 'string_node', unescaped: { last: 'x' } } } } } do
+      width = node.arguments.arguments.first.resize_value.to_value.to_i
+      replace 'arguments.arguments.first.resize_element', with: "resize_to_limit: [#{width}, nil]"
     end
 
-    with_node node_type: 'send', message: 'variant', arguments: { size: 1, first: { node_type: 'hash', crop_value: { node_type: 'str' } } } do
-      width, height, x, y = node.arguments.first.crop_value.to_value.split(/x|\+/)
+    with_node node_type: 'call_node',
+              name: 'variant',
+              arguments: { node_type: 'arguments_node', arguments: { size: 1, first: { node_type: 'keyword_hash_node', crop_value: { node_type: 'string_node' } } } } do
+      width, height, x, y = node.arguments.arguments.first.crop_value.to_value.split(/x|\+/)
       replace :arguments, with: "crop: [#{x}, #{y}, #{width}, #{height}]"
     end
 
-    with_node node_type: 'send', message: 'variant', arguments: { size: 1, first: { node_type: 'hash', resize_and_pad_value: { node_type: 'array', elements: { size: 2 } } } } do
-      insert 'background: [255]', to: 'arguments.0.pairs.0.value.elements.-1', at: 'end', and_comma: true
+    with_node node_type: 'call_node',
+              name: 'variant',
+              arguments: { node_type: 'arguments_node', arguments: { size: 1, first: { node_type: 'keyword_hash_node', resize_and_pad_value: { node_type: 'array_node', elements: { size: 2 } } } } } do
+      insert 'background: [255]', to: 'arguments.arguments.0.elements.0.value.elements.-1', at: 'end', and_comma: true
     end
 
-    with_node node_type: 'send', message: 'variant', arguments: { size: 1, first: { node_type: 'hash', monochrome_value: true } } do
-      replace 'arguments.first.monochrome_pair', with: 'colourspace: "b-w"'
+    with_node node_type: 'call_node',
+              name: 'variant',
+              arguments: { node_type: 'arguments_node', arguments: { size: 1, first: { node_type: 'keyword_hash_node', monochrome_value: true } } } do
+      replace 'arguments.arguments.0.monochrome_element', with: 'colourspace: "b-w"'
     end
   end
 end
