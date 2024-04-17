d48d722fda55cf59187ceabc7de24372f733f88e
Richard Huang <flyerhzm@gmail.com>
2024-04-17 18:53:08 +0800

    use PRISM_PARSER in rails/convert_rails_logger

--- lib/rails/convert_rails_logger.rb
+++ lib/rails/convert_rails_logger.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'convert_rails_logger' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It converts RAILS_DEFAULT_LOGGER to Rails.logger.
@@ -20,10 +20,7 @@ Synvert::Rewriter.new 'rails', 'convert_rails_logger' do
   if_gem 'rails', '>= 2.3'
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
-    with_node node_type: 'const', to_source: 'RAILS_DEFAULT_LOGGER' do
-      replace_with 'Rails.logger'
-    end
-    with_node node_type: 'const', to_source: '::RAILS_DEFAULT_LOGGER' do
+    with_node node_type: 'constant_read_node', name: 'RAILS_DEFAULT_LOGGER' do
       replace_with 'Rails.logger'
     end
   end
--- spec/rails/convert_rails_logger_spec.rb
+++ spec/rails/convert_rails_logger_spec.rb
@@ -7,11 +7,9 @@ RSpec.describe 'Upgrade RAILS_DEFAULT_LOGGER to Rails.logger' do
   let(:fake_file_path) { 'config/initializers/constant.rb' }
   let(:test_content) { <<~EOS }
     RAILS_DEFAULT_LOGGER
-    ::RAILS_DEFAULT_LOGGER
   EOS
 
   let(:test_rewritten_content) { <<~EOS }
-    Rails.logger
     Rails.logger
   EOS
 
