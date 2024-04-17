dc80c67a8ccdb493530c11140d0f1019f1289fa5
Richard Huang <flyerhzm@gmail.com>
2024-04-17 19:05:31 +0800

    do not warn callback return

--- lib/rails/upgrade_4_0_to_4_1.rb
+++ lib/rails/upgrade_4_0_to_4_1.rb
@@ -10,17 +10,4 @@ Synvert::Rewriter.new 'rails', 'upgrade_4_0_to_4_1' do
   add_snippet 'rails', 'convert_configs_4_0_to_4_1'
   add_snippet 'rails', 'deprecate_active_record_migration_check_pending'
   add_snippet 'rails', 'deprecate_multi_json'
-
-  if_gem 'rails', '>= 4.1'
-
-  within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
-    [/before_/, /after_/].each do |message_regex|
-      # Warn if finding return in before_* or after_* callbacks
-      within_node node_type: 'block', caller: { node_type: 'send', message: message_regex } do
-        with_node node_type: 'return' do
-          warn 'Using a return statement in an inline callback block causes a LocalJumpError to be raised when the callback is executed.'
-        end
-      end
-    end
-  end
 end
