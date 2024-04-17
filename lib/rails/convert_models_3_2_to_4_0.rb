9e989fe4203db4107d3697f1accc9a034a683a34
Richard Huang <flyerhzm@gmail.com>
2024-04-17 17:43:44 +0800

    use PRISM_PARSER in rails/convert_models_3_2_to_4_0

--- lib/rails/convert_models_3_2_to_4_0.rb
+++ lib/rails/convert_models_3_2_to_4_0.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'convert_models_3_2_to_4_0' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It converts rails models from 3.2 to 4.0.
@@ -25,17 +25,16 @@ Synvert::Rewriter.new 'rails', 'convert_models_3_2_to_4_0' do
 
   within_files Synvert::RAILS_MODEL_FILES do
     # self.serialized_attributes => self.class.serialized_attributes
-    with_node node_type: 'send', receiver: 'self', message: 'serialized_attributes' do
-      replace_with 'self.class.serialized_attributes'
+    with_node node_type: 'call_node', receiver: 'self', name: 'serialized_attributes' do
+      insert 'class.', to: 'message', at: 'beginning'
     end
 
     # has_many :comments, dependent: :restrict => has_many :comments, dependent: restrict_with_exception
-    %w[has_one has_many].each do |message|
-      within_node node_type: 'send', receiver: nil, message: message do
-        with_node node_type: 'pair', key: 'dependent', value: :restrict do
-          replace_with 'dependent: :restrict_with_exception'
-        end
-      end
+    within_node node_type: 'call_node',
+                receiver: nil,
+                name: { in: ['has_one', 'has_many'] },
+                arguments: { node_type: 'arguments_node', arguments: { last: { node_type: 'keyword_hash_node', dependent_value: :restrict } } } do
+      replace 'arguments.arguments.-1.dependent_value', with: ':restrict_with_exception'
     end
   end
 end
