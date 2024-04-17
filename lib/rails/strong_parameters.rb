58c8083d574ffd19c26e4c8a78a33151d73b8b4f
Richard Huang <flyerhzm@gmail.com>
2024-04-17 17:54:51 +0800

    use PRISM_PARSER in rails/strong_parameters

--- lib/rails/strong_parameters.rb
+++ lib/rails/strong_parameters.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'rails', 'strong_parameters' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   default_columns = %w[id created_at updated_at deleted_at]
 
@@ -36,30 +36,24 @@ Synvert::Rewriter.new 'rails', 'strong_parameters' do
 
   within_files 'config/**/*.rb' do
     # remove config.active_record.whitelist_attributes = ...
-    with_node node_type: 'send',
+    with_node node_type: 'call_node',
               receiver: {
-                node_type: 'send',
-                receiver: {
-                  node_type: 'send',
-                  message: 'config'
-                },
-                message: 'active_record'
+                node_type: 'call_node',
+                receiver: 'config',
+                name: 'active_record'
               },
-              message: 'whitelist_attributes=' do
+              name: 'whitelist_attributes=' do
       remove
     end
 
     # remove config.active_record.mass_assignment_sanitizer = ...
-    with_node node_type: 'send',
+    with_node node_type: 'call_node',
               receiver: {
-                node_type: 'send',
-                receiver: {
-                  node_type: 'send',
-                  message: 'config'
-                },
-                message: 'active_record'
+                node_type: 'call_node',
+                receiver: 'config',
+                name: 'active_record'
               },
-              message: 'mass_assignment_sanitizer=' do
+              name: 'mass_assignment_sanitizer=' do
       remove
     end
   end
@@ -69,19 +63,19 @@ Synvert::Rewriter.new 'rails', 'strong_parameters' do
 
   parameters = {}
   within_files Synvert::RAILS_MODEL_FILES do
-    within_node node_type: 'class' do
-      object_name = node.name.to_source.underscore
+    within_node node_type: 'class_node' do
+      object_name = node.name.to_s.underscore
       table_columns = rails_tables.present? ? rails_tables[object_name.tableize][:columns].map { |column| column[:name] } - default_columns : []
 
       # assign and remove attr_accessible ...
-      with_node node_type: 'send', message: 'attr_accessible' do
-        parameters[object_name] = node.arguments.map(&:to_source)
+      with_node node_type: 'call_node', name: 'attr_accessible' do
+        parameters[object_name] = node.arguments.arguments.map(&:to_source)
         remove
       end
 
       # assign and remove attr_protected ...
-      with_node node_type: 'send', message: 'attr_protected' do
-        parameters[object_name] = table_columns.map { |column| ":#{column}" } - node.arguments.map(&:to_source)
+      with_node node_type: 'call_node', name: 'attr_protected' do
+        parameters[object_name] = table_columns.map { |column| ":#{column}" } - node.arguments.arguments.map(&:to_source)
         remove
       end
     end
@@ -89,13 +83,13 @@ Synvert::Rewriter.new 'rails', 'strong_parameters' do
 
 
   within_file Synvert::RAILS_CONTROLLER_FILES do
-    within_node node_type: 'class' do
-      object_name = node.name.to_source.sub('Controller', '').singularize.underscore
-      if_exist_node node_type: 'send', receiver: 'params', message: '[]', arguments: [object_name.to_sym] do
+    within_node node_type: 'class_node' do
+      object_name = node.name.to_s.sub('Controller', '').singularize.underscore
+      if_exist_node node_type: 'call_node', receiver: 'params', name: '[]', arguments: { node_type: 'arguments_node', arguments: [object_name.to_sym] } do
         if parameters[object_name]
           # append def xxx_params; ...; end
           permit_params = parameters[object_name].join(', ')
-          unless_exist_node node_type: 'def', name: "#{object_name}_params" do
+          unless_exist_node node_type: 'def_node', name: "#{object_name}_params" do
             append <<~EOS
               def #{object_name}_params
                 params.require(:#{object_name}).permit(#{permit_params})
@@ -104,8 +98,8 @@ Synvert::Rewriter.new 'rails', 'strong_parameters' do
           end
 
           # params[:xxx] => xxx_params
-          with_node node_type: 'send', receiver: 'params', message: '[]' do
-            object_name = node.arguments.first.to_value.to_s
+          with_node node_type: 'call_node', receiver: 'params', name: '[]' do
+            object_name = node.arguments.arguments.first.to_value.to_s
             if parameters[object_name]
               replace_with "#{object_name}_params"
             end
