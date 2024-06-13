1c4f9769d4f3b664f4522b0ed51283514231e5b2
Richard Huang <flyerhzm@gmail.com>
2024-06-13 14:52:37 +0800

    use PRISM_PARSER in ruby/map_and_flatten_to_flat_map

--- lib/ruby/map_and_flatten_to_flat_map.rb
+++ lib/ruby/map_and_flatten_to_flat_map.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Synvert::Rewriter.new 'ruby', 'map_and_flatten_to_flat_map' do
-  configure(parser: Synvert::PARSER_PARSER)
+  configure(parser: Synvert::PRISM_PARSER)
 
   description <<~EOS
     It converts `map` and `flatten` to `flat_map`
 
     ```ruby
-    enum.map do
+    enum.map do |item|
       # do something
     end.flatten
     ```
@@ -15,24 +15,24 @@ Synvert::Rewriter.new 'ruby', 'map_and_flatten_to_flat_map' do
     =>
 
     ```ruby
-    enum.flat_map do
+    enum.flat_map do |item|
       # do something
     end
     ```
   EOS
 
   within_files Synvert::ALL_RUBY_FILES + Synvert::ALL_RAKE_FILES do
-    # enum.map do
+    # enum.map do |item|
     #   # do something
     # end.flatten
     # =>
-    # enum.flat_map do
+    # enum.flat_map do |item|
     #   # do something
     # end
-    find_node '.send [receiver=.block [caller=.send[message=map]]] [message=flatten] [arguments.size=0]' do
+    find_node '.call_node[receiver=.call_node[name=map][arguments=nil][block=.block_node]][name=flatten][arguments=nil]' do
       group do
-        delete :message, :dot
-        replace 'receiver.caller.message', with: 'flat_map'
+        delete :call_operator, :name
+        replace 'receiver.name', with: 'flat_map'
       end
     end
   end
