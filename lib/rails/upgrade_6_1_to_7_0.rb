b41909b176a1cdfd141bd1a4d62eb4c84b349f6b
Richard Huang <flyerhzm@gmail.com>
2024-06-15 17:11:51 +0800

    add rails/deprecate_errors_as_hash to rails/upgrade_6_1_to_7_0

--- lib/rails/upgrade_6_1_to_7_0.rb
+++ lib/rails/upgrade_6_1_to_7_0.rb
@@ -4,6 +4,7 @@ Synvert::Rewriter.new 'rails', 'upgrade_6_1_to_7_0' do
   description 'It upgrades rails 6.1 to 7.0.'
 
   add_snippet 'rails', 'convert_configs_6_1_to_7_0'
+  add_snippet 'rails', 'deprecate_errors_as_hash'
   add_snippet 'rails', 'remove_active_support_dependencies_private_api'
   add_snippet 'rails', 'update_active_storage_variant_argument'
 end
