7fdf444d1f72347283040e6ea2790a25b1ab8b86
Awesome Code <team@xinminlabs.com>
2024-07-06 08:33:58 +0000

    Auto corrected by following Format Ruby Code

--- lib/ruby/prefer-endless-method.rb
+++ lib/ruby/prefer-endless-method.rb
@@ -28,11 +28,20 @@ Synvert::Rewriter.new 'ruby', 'prefer-endless-method' do
 
       first_body_node = node.body.body.first
       break if %i[if_node unless_node].include?(first_body_node.type) && first_body_node.end_keyword.nil?
-      break if %i[multi_write_node instance_variable_or_write_node class_variable_or_write_node or_node and_node hash_node].include?(first_body_node.type)
+      break if %i[
+        multi_write_node
+        instance_variable_or_write_node
+        class_variable_or_write_node
+        or_node
+        and_node
+        hash_node
+      ].include?(first_body_node.type)
+
       break if first_body_node.type == :call_node && first_body_node.opening.nil? && !first_body_node.arguments.nil? && first_body_node.closing.nil? && !first_body_node.block.nil?
 
       body_column = mutation_adapter.get_start_loc(first_body_node).column
-      new_body = first_body_node.to_source.split("\n").map { |line| line.sub(/^ {#{body_column}}/, '') }.join("\n")
+      new_body = first_body_node.to_source.split("\n").map { |line| line.sub(/^ {#{body_column}}/, '') }
+                                .join("\n")
       receiver_and_name = node.receiver ? "#{node.receiver.to_source}.#{node.name}" : node.name.to_s
       replace_with "def #{receiver_and_name}{{lparen}}{{parameters}}{{rparen}} = #{new_body}"
     end
