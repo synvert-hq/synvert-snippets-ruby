6c9ec6cef70551e0b227e3534a1e6e9a225888ec
Richard Huang <flyerhzm@gmail.com>
2024-12-14 16:21:27 +0800

    add rails/new_enum_syntax to rails/upgrade_7_0_to_7_1

--- lib/rails/upgrade_7_0_to_7_1.rb
+++ lib/rails/upgrade_7_0_to_7_1.rb
@@ -4,4 +4,5 @@ Synvert::Rewriter.new 'rails', 'upgrade_7_0_to_7_1' do
   description 'It upgrades rails 7.0 to 7.1.'
 
   add_snippet 'rails', 'convert_configs_7_0_to_7_1'
+  add_snippet 'rails', 'new_enum_syntax'
 end
